UUDigitalHumanitieslab/readit-interface

commit f2ae3192aaeec5815b4f941d6dd8a3fe0fdc1010

Replace magic strings by neatly normalized constants (#1)

frontend/gulpfile.ts
@@ -12,38 +12,53 @@ import del = require('del');
 import loadPlugins = require('gulp-load-plugins');
 const plugins = loadPlugins();
 
+const sourceDir = 'src',
+    buildDir = 'build',
+    distDir = 'dist',
+    nodeDir = 'node_modules',
+    mainScript = `${sourceDir}/main.ts`,
+    jsBundleName = 'bundle.js',
+    jsSourceMapDest = `${buildDir}/bundle.js.map`,
+    jsTargetVersion = 'es5',
+    jsModuleType = 'commonjs',
+    templateSourceGlob = `${sourceDir}/**/*-template.hbs`,
+    templateOutputGlob = `${sourceDir}/**/*-template.js`,
+    styleDir = `${sourceDir}/style`,
+    mainStylesheet = '${styleDir}/main.sass',
+    styleSourceGlob = '${styleDir}/*.sass';
+
 function tsModules(debug = true) {
     return browserify({
         debug: debug,
-        entries: ['src/main.ts'],
+        entries: [mainScript],
         cache: {},
         packageCache: {},
     }).plugin(tsify, {
-        target: 'es5',
+        target: jsTargetVersion,
     });
 }
 
 function jsbundle(modules, optimize = false) {
     if (optimize) return function() {
         return modules.bundle()
-            .pipe(vinylStream('bundle.js'))
+            .pipe(vinylStream(jsBundleName))
             .pipe(vinylBuffer())
             .pipe(plugins.uglify())
-            .pipe(gulp.dest('dist'));
+            .pipe(gulp.dest(distDir));
     }; else return function() {
         return modules.bundle()
-            .pipe(exorcist('build/bundle.js.map'))
-            .pipe(vinylStream('bundle.js'))
-            .pipe(gulp.dest('build'));
+            .pipe(exorcist(jsSourceMapDest))
+            .pipe(vinylStream(jsBundleName))
+            .pipe(gulp.dest(buildDir));
     };
 }
 
 function style(optimize = false) {
     let postcssPlugins = [autoprefixer()];
     if (optimize) postcssPlugins.push(cssnano());
-    let stream = gulp.src('src/style/main.sass');
+    let stream = gulp.src(mainStylesheet);
     if (!optimize) stream = stream.pipe(plugins.sourcemaps.init());
-    stream = stream.pipe(plugins.sass({includePaths: ['node_modules']}))
+    stream = stream.pipe(plugins.sass({includePaths: [nodeDir]}))
         .pipe(plugins.postcss(postcssPlugins));
     if (!optimize) stream = stream.pipe(plugins.sourcemaps.write('.'));
     return stream;
@@ -54,22 +69,22 @@ gulp.task('ts', ['hbs'], jsbundle(tsModules()));
 gulp.task('ts:dist', ['hbs'], jsbundle(tsModules(false), true));
 
 gulp.task('sass', function() {
-    return style().pipe(gulp.dest('build'));
+    return style().pipe(gulp.dest(buildDir));
 });
 
 gulp.task('sass:dist', function() {
-    return style(true).pipe(gulp.dest('dist'));
+    return style(true).pipe(gulp.dest(distDir));
 });
 
 gulp.task('hbs', function() {
-    return gulp.src('src/**/*-template.hbs')
+    return gulp.src(templateSourceGlob)
         .pipe(plugins.handlebars({
             compilerOptions: {
                 knownHelpers: {},
                 knownHelpersOnly: true,
             },
         }))
-        .pipe(plugins.defineModule('commonjs', {
+        .pipe(plugins.defineModule(jsModuleType, {
             require: {Handlebars: 'handlebars/runtime'},
         }))
         .pipe(gulp.dest('src'));
@@ -81,12 +96,12 @@ gulp.task('watch', ['sass', 'hbs'], function() {
     tsModulesWatched.on('update', bundleWatched);
     tsModulesWatched.on('log', log);
     bundleWatched();
-    gulp.watch('src/style/*.sass', ['sass']);
-    gulp.watch('src/**/*-template.hbs', ['hbs']);
+    gulp.watch(styleSourceGlob, ['sass']);
+    gulp.watch(templateSourceGlob, ['hbs']);
 });
 
 gulp.task('clean', function() {
-    return del(['build', 'dist', 'src/**/*-template.js']);
+    return del([buildDir, distDir, templateOutputGlob]);
 });
 
 gulp.task('default', function() {

commit 91282198a47d99ab28140127e16bb43d3a16ca78

Fix the issues with the constants (#1)
The sass task must have been broken since f2ae3192. All constants have
preventively been changed into interpolation strings.

frontend/gulpfile.ts
@@ -14,20 +14,20 @@ import yargs = require('yargs');
 import loadPlugins = require('gulp-load-plugins');
 const plugins = loadPlugins();
 
-const sourceDir = 'src',
-    buildDir = 'dist',
-    nodeDir = 'node_modules',
+const sourceDir = `src`,
+    buildDir = `dist`,
+    nodeDir = `node_modules`,
     mainScript = `${sourceDir}/main.ts`,
-    jsBundleName = 'bundle.js',
+    jsBundleName = `bundle.js`,
     jsSourceMapDest = `${buildDir}/bundle.js.map`,
-    jsTargetVersion = 'es5',
-    jsModuleType = 'commonjs',
+    jsTargetVersion = `es5`,
+    jsModuleType = `commonjs`,
     templateSourceGlob = `${sourceDir}/**/*-template.hbs`,
     templateOutputGlob = `${sourceDir}/**/*-template.js`,
     styleDir = `${sourceDir}/style`,
-    mainStylesheet = '${styleDir}/main.sass',
-    styleSourceGlob = '${styleDir}/*.sass',
-    indexConfig = yargs.argv.config || 'config.json',
+    mainStylesheet = `${styleDir}/main.sass`,
+    styleSourceGlob = `${styleDir}/*.sass`,
+    indexConfig = yargs.argv.config || `config.json`,
     indexTemplate = `${sourceDir}/index.hbs`,
     production = yargs.argv.production || false;
 
@@ -83,7 +83,7 @@ gulp.task('hbs', function() {
         .pipe(plugins.defineModule(jsModuleType, {
             require: {Handlebars: 'handlebars/runtime'},
         }))
-        .pipe(gulp.dest('src'));
+        .pipe(gulp.dest(sourceDir));
 });
 
 gulp.task('index', function(done) {