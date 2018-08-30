a9411e56228e48228ffa4380f3aa4e95d551f435
Julian Gonggrijp <dev@juliangonggrijp.com>
2018-08-30 16:00:02 +0200

    Fix a typo

--- frontend/src/aspects/example.ts
+++ frontend/src/aspects/example.ts
@@ -4,6 +4,6 @@ import { i18nPromise } from '../global/i18n';
 
 // Normally the promise wouldn't be required. It is only used in this
 // example because we are not waiting for bb.history.start() before
-// rendering. This will be fixed in a latter commit.
+// rendering. This will be fixed in a later commit.
 i18nPromise.then(() => exampleView.render().$el.appendTo(document.body));
 exampleView.listenTo(example, 'change:property1', exampleView.render);
