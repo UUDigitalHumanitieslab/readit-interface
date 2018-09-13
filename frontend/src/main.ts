35087ecf4315f17de02a504661809c693a5d4201
Julian Gonggrijp <dev@juliangonggrijp.com>
2018-09-13 22:06:44 +0200

    Lift the Promise polyfill into its own package (#1)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,8 @@
 import * as bb from 'backbone';
 import * as $ from 'jquery';
+import '@dhl-uu/jquery-promise';
 
 import { i18nPromise } from './global/i18n';
-import './global/promise-shim';
 import './aspects/example';
 
 $.when($.ready, i18nPromise).done(function() {
--- frontend/src/test-util.ts
+++ frontend/src/test-util.ts
@@ -1,2 +1,3 @@
-import './global/promise-shim';
+import '@dhl-uu/jquery-promise';
+
 export { i18nPromise } from './global/i18n';
