0fd4804b404ba9b595e04c16f9de4eb5d540558f
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-09-18 20:04:13 +0200

    Import the config in the main.ts to set the root in bb.history (#1)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,12 @@ import * as bb from 'backbone';
 import * as $ from 'jquery';
 import '@dhl-uu/jquery-promise';
 
+import * as config from 'config.json';
 import { i18nPromise } from './global/i18n';
 import './aspects/example';
 
 $.when($.ready, i18nPromise).done(function() {
-    bb.history.start();
+    let success = bb.history.start({
+        root: config.baseUrl,
+    });
 });
