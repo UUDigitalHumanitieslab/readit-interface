77a51dab351974330c5e36ac87bb192723e4d204
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-09-19 18:06:56 +0200

    Use non-star imports where possible (#1)

--- frontend/src/global/i18n.ts
+++ frontend/src/global/i18n.ts
@@ -1,4 +1,4 @@
-import * as $ from 'jquery';
+import { Deferred } from 'jquery';
 import * as i18next from 'i18next/dist/umd/i18next';
 import * as languageDetector from 'i18next-browser-languagedetector';
 import Handlebars from 'handlebars/dist/handlebars.runtime';
@@ -6,7 +6,7 @@ import registerI18nHelper from 'handlebars-i18next';
 
 import * as french from '../i18n/fr.json';
 
-const deferred = $.Deferred();
+const deferred = Deferred();
 const i18nPromise = deferred.promise();
 
 i18next.use(
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,13 @@
-import * as bb from 'backbone';
-import * as $ from 'jquery';
+import { history } from 'backbone';
+import { when, ready } from 'jquery';
 import '@dhl-uu/jquery-promise';
 
 import * as config from 'config.json';
 import { i18nPromise } from './global/i18n';
 import './aspects/example';
 
-$.when($.ready, i18nPromise).done(function() {
-    let success = bb.history.start({
+when(ready, i18nPromise).done(function() {
+    let success = history.start({
         root: config.baseUrl,
     });
 });
