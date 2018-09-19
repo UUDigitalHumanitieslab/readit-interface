d5a0d66adcdb5010322d2eaa263df859907f95b9
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-09-19 11:41:01 +0200

    Fix asynchronicity in Jasmine test

--- frontend/src/example/example-view-test.ts
+++ frontend/src/example/example-view-test.ts
@@ -6,9 +6,8 @@ describe('ExampleView', function() {
     beforeEach(function() {
         this.view = new ExampleView();
     });
-    it('renders a simple greeting message', async function(done) {
+    it('renders a simple greeting message', async function() {
         await i18nPromise;
         expect(this.view.render().$el.html()).toContain('Alex');
-        done();
     });
 });
