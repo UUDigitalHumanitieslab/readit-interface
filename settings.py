c9bd571a77b4161782471d050761795cd69ceeed
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-01-02 16:52:20 +0100

    Fix Python 3.4 compatibility (fix #5)

--- settings.py
+++ settings.py
@@ -11,15 +11,23 @@ here = op.dirname(op.abspath(__file__))
 # First, import the standard backend settings. This requires some
 # magic because the backend directory itself is not a Python package.
 # Imitated from https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+# or
+# https://stackoverflow.com/a/29855240
+# (respectively for Python >= 3.5 and Python 3.4)
 
 import sys
-from importlib import util
+from importlib import util, machinery
 
 settings_name = 'settings'
 settings_path = op.join(here, 'backend', 'readit', 'settings.py')
-spec = util.spec_from_file_location(settings_name, settings_path)
-settings = util.module_from_spec(spec)
-spec.loader.exec_module(settings)
+
+if sys.version_info >= (3, 5):
+    spec = util.spec_from_file_location(settings_name, settings_path)
+    settings = util.module_from_spec(spec)
+    spec.loader.exec_module(settings)
+else:
+    settings = machinery.SourceFileLoader(settings_name, settings_path).load_module()
+
 sys.modules[settings_name] = settings
 
 from settings import *
