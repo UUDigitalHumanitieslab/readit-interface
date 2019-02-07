f14dedb8746f438f52af581c2440438971e4cb60
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-02-07 17:35:04 +0100

    Add JSON typings (#11)

---
+++ frontend/src/core/json.ts
@@ -0,0 +1,7 @@
+export type JsonValue = null | boolean | number | string | JsonObject | JsonArray;
+export type JsonArray = JsonValue[];
+export interface JsonObject {
+    [key: string]: JsonValue;
+}
+export type JsonCollection = JsonObject[];
+export type JsonDocument = JsonObject | JsonCollection;
