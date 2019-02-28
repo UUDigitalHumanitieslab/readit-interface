fb1915d89c450fb454ffddf0545bc86fae755e00
Jelte van Boheemen <j.vanboheemen@gmail.com>
2019-02-28 12:11:29 +0100

    update selection on click

--- frontend/src/search/search-box/searchbox-view.ts
+++ frontend/src/search/search-box/searchbox-view.ts
@@ -5,19 +5,19 @@ import searchboxTemplate from './searchbox-template';
 import QueryField from './query-field'
 
 export default class SearchboxView extends View {
-    placeholder: string;
+    defaultOption: QueryField;
     queryFields: QueryField[] = undefined;
     /**
      * Ctor for SearchboxView
      * @param queryFields Set of fields to include in the query
-     * @param placeholder Defaults to 'in fields..'
+     * @param initial Option that is initially selected. Defaults to first option.
      */
     constructor(
         queryFields: QueryField[],
-        placeholder: string = "in fields..") {
+        defaultOption: QueryField = queryFields[0]) {
         super()
         this.queryFields = queryFields;
-        this.placeholder = placeholder;
+        this.defaultOption = defaultOption;
     }
 
     render() {
@@ -37,8 +37,16 @@ export default class SearchboxView extends View {
 
     search(event: any) {
         event.preventDefault();
-        var query = this.$('.input').val();
+        var query = this.$(".input").val();
+        var query_fields = this.$(".dropdown-item .is-active").val()
         this.trigger("searchClicked", query)
+    }
+
+    selectField(event: any) {
+        var target = $(event.target);
+        $("#selected-option").html(target.html());
+        $(".dropdown-item").removeClass("is-active");
+        target.addClass("is-active");
     }
 }
 
@@ -48,6 +56,7 @@ extend(SearchboxView.prototype, {
     template: searchboxTemplate,
     events: {
         "click .button": "search",
+        "click .dropdown-item": "selectField",
         "keyup input": "onKeyUp",
     }
 });
