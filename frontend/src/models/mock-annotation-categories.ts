a166beabf6a607c7c54ee42f8742c45f1fb38510
Alex Hebing <a.h.j.hebing@uu.nl>
2019-03-11 10:33:48 +0100

    Color classes moved to mock data

---
+++ frontend/src/models/mock-annotation-categories.ts
@@ -0,0 +1,92 @@
+var mockAnnotationCategories = [    
+    {
+        id: 9001,
+        name: 'reader',
+        attributes: [
+            {
+                id: 20003,
+                name: 'birth name',
+            },
+            {
+                id: 20004,
+                name: 'gender',
+            },
+            {
+                id: 20005,
+                name: 'place of birth',
+            },
+            {
+                id: 20006,
+                name: 'date of birth',
+            },
+            {
+                id: 20007,
+                name: 'place of death',
+            },
+            {
+                id: 20008,
+                name: 'date of death',
+            },
+            {
+                id: 21000,
+                name: 'name',
+            },
+            {
+                id: 21001,
+                name: 'age'
+            },
+            {
+                id: 21002,
+                name: 'language',
+            },
+            {
+                id: 21003,
+                name: 'occupation',
+            },
+            {
+                id: 21004,
+                name: 'education',
+            },
+            {
+                id: 21005,
+                name: 'political position',
+            },
+        ]
+    },
+    {
+        id: 9002,
+        name: 'support'        
+    },
+    {
+        id: 9003,
+        name: 'content',
+    },
+    {
+        id: 9004,
+        name: 'state_of_mind',
+        attributes: [
+            {
+                id: 21006,
+                name: 'aim',
+            },
+            {
+                id: 21007,
+                name: 'self-reflection',
+            },
+            {
+                id: 21008,
+                name: 'emotion',
+            },
+            {
+                id: 21009,
+                name: 'remembrance',
+            },
+            {
+                id: 21010,
+                name: 'achievement',
+            },
+        ]
+    },
+]
+
+export default mockAnnotationCategories;
