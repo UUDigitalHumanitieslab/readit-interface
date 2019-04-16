a353e54a8a8d088fd877ee94b794be1ec9e1fb3a
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-04-16 17:30:18 +0200

    Rewrite the direction FSM to extend the user base FSM (#19)

--- frontend/src/example/direction-fsm.ts
+++ frontend/src/example/direction-fsm.ts
@@ -1,19 +1,43 @@
-import Fsm from '../core/fsm';
+import { defaults } from 'lodash';
 
-const DirectionFsm = Fsm.extend({
+import AuthorizationFsm, { requireAuthorization } from '../user/user-fsm-base';
+
+/**
+ * Default transitions for states that don't require authorization.
+ * See the "arriving" state below for an example of how to extend
+ * this.
+ */
+const unprivilegedState = {
+    arrive: requireAuthorization,
+    leave: 'leaving',
+};
+
+/**
+ * Default transitions for states that require authorization. See
+ * the "arriving" state below for an example of how to extend this.
+ */
+const privilegedState = {
+    arrive: 'arriving',
+    leave: 'leaving',
+};
+
+/**
+ * The main states that the user can occupy.
+ * Authorization-aware, privilege levels could be more fine-grained.
+ * @class
+ */
+export default AuthorizationFsm.extend({
     initialState: 'travelling',
+    // Deep-extend, so includes the states and transitions from
+    // AuthorizationFsm.
     states: {
-        travelling: {
-            arrive: 'arriving',
-            leave: 'leaving',
-        },
-        arriving: {
-            leave: 'leaving',
-        },
-        leaving: {
-            arrive: 'arriving',
-        },
+        travelling: unprivilegedState,
+        arriving: defaults({
+            // Special case for the arriving state.
+            // Other privileged states may have different logout handlers.
+            logout: 'leaving',
+        }, privilegedState),
+        leaving: unprivilegedState,
+        authorizationGranted: privilegedState,
     },
 });
-
-export default DirectionFsm;
