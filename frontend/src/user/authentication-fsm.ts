aff8c90be4aecb7c7391c3123b9d1ea82778bab7
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-04-16 16:55:15 +0200

    Simplify the AuthenticationFsm (#19)

--- frontend/src/user/authentication-fsm.ts
+++ frontend/src/user/authentication-fsm.ts
@@ -3,41 +3,22 @@ import { noop } from 'lodash';
 import Fsm from '../core/fsm';
 
 /**
- * Make this FSM the top of a hierarchy. The unauthenticated and
- * authenticated states should each get a _child FSM that provides
- * the available states for that condition.
+ * Simple FSM that represents whether a user has authenticated or not.
  * @class
  */
 export default Fsm.extend({
     initialState: 'unauthenticated',
     states: {
         unauthenticated: {
-            // Forget about any previously deferred inputs.
-            _onEnter: function() {
-                this.clearQueue();
-            },
-            // By default, forward to login on any input.
-            // Exceptions can be made by extending with a _child when
-            // creating an instance.
-            '*': function() {
-                this.deferAndTransition('attemptLogin');
-            },
+            login: 'attemptLogin',
         },
         attemptLogin: {
             loginSuccess: 'authenticated',
             loginFail: noop,
             loginCancel: 'unauthenticated',
-            // Keep deferred inputs around so they replay again when
-            // we enter another state.
-            '*': function() {
-                this.deferUntilTransition();
-            },
         },
         authenticated: {
             logout: 'unauthenticated',
-            // By default, this is a no-op sink for all inputs.
-            // Extend with a _child to make something happen when
-            // creating an instance.
         },
     },
 });
