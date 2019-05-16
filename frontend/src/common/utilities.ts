b53f70b26d4b3bfbd5121c8ea3b4506f37892bb1
Julian Gonggrijp <dev@juliangonggrijp.com>
2019-05-16 14:02:11 +0200

    Trim trailing whitespace
    @alexhebing it appears that your editor is ignoring the
    .editorconfig.

--- frontend/src/common/utilities.ts
+++ frontend/src/common/utilities.ts
@@ -2,12 +2,12 @@ import Node from './../jsonld/node';
 
 export function getLabel(node: Node): string {
     let keys = ['skos:prefLabel', 'rdfs:label', 'skos:altLabel'];
-    
+
     for (let i = 0; i < keys.length - 1; i++) {
         let label = node.get(keys[i]);
-        
+
         if (label) {
-            return label; 
+            return label;
         }
     }
 
@@ -15,10 +15,10 @@ export function getLabel(node: Node): string {
 }
 
 export function getCssClassName(node: Node): string {
-    if (node.get('@type') !== 'rdfs:Class') return; 
-    
+    if (node.get('@type') !== 'rdfs:Class') return;
+
     let label = getLabel(node);
-    
+
     if (label) {
         label = label.replace(new RegExp(' ', 'g'), '');
         return `is_readit-${label}`;
