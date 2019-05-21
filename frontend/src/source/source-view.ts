6d8dd5d8f79f4aab9fe1cc23f3da6b46fc3f8736
Alex Hebing <a.h.j.hebing@uu.nl>
2019-05-21 13:21:14 +0200

    Some styling and events added (#34)

--- frontend/src/source/source-view.ts
+++ frontend/src/source/source-view.ts
@@ -7,7 +7,16 @@ export default class SourceView extends View {
 
     render() {
         this.$el.html(this.template(this));
+
+        // TODO: if panel mode
+        this.$el.addClass('explorer-panel');
+
         return this;
+    }
+
+    onToolbarItemClicked(event: any) {
+        let clickedItem = $(event.currentTarget).data('toolbar');
+        this.trigger('toolbarClicked', clickedItem);
     }
 }
 extend(SourceView.prototype, {
@@ -15,5 +24,6 @@ extend(SourceView.prototype, {
     className: 'source',
     template: sourceTemplate,
     events: {
+        'click .toolbar-item': 'onToolbarItemClicked',
     }
 });
