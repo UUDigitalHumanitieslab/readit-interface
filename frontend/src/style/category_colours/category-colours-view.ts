e48f38d3db6f970f18ef4d398f5cc2d8efa4eeb6
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-05-21 17:58:01 +0200

    Make TypeScript aware that CategoryColorsView has a Graph (#72)

--- frontend/src/style/category_colours/category-colours-view.ts
+++ frontend/src/style/category_colours/category-colours-view.ts
@@ -1,12 +1,24 @@
 import { extend } from 'lodash';
+import { ViewOptions as BaseOpt } from 'backbone';
+
 import View from './../../core/view';
 import Node from './../../jsonld/node';
+import Graph from './../../jsonld/graph';
 
 import categoryColoursTemplate from './category-colours-template';
 import { getCssClassName } from './../../common/utilities';
 
+export interface ViewOptions extends BaseOpt<Node> {
+    collection: Graph;
+}
+
 export default class CategoryColoursView extends View {
+    collection: Graph
     private categoryColours;
+
+    constructor(options: ViewOptions) {
+        super(options);
+    }
 
     initialize(): void {
         this.categoryColours = [];
@@ -21,7 +33,7 @@ export default class CategoryColoursView extends View {
 
     collectColours(): void {
         this.collection.each(node => {
-            let cssClass = getCssClassName(node as Node);
+            let cssClass = getCssClassName(node);
             this.categoryColours.push({ 'class': cssClass, 'colour': node.get('schema:color') });
         });
     }
