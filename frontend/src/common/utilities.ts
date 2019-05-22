8b9a441ccc9cda4b1e2048bc476bd9bddfcc4ca9
Alex Hebing <30618796+alexhebing@users.noreply.github.com>
2019-05-22 08:32:30 +0200

    Update frontend/src/common/utilities.ts
    Co-Authored-By: Julian Gonggrijp <dev@juliangonggrijp.com>

--- frontend/src/common/utilities.ts
+++ frontend/src/common/utilities.ts
@@ -3,7 +3,7 @@ import Node from './../jsonld/node';
 export function getLabel(node: Node): string {
     let keys = ['skos:prefLabel', 'rdfs:label', 'skos:altLabel'];
 
-    for (let i = 0; i < keys.length - 1; i++) {
+    keys.forEach(key => {
         let label = node.get(keys[i]);
 
         if (label) {
