f51b7d1e6fd43f6fbdf684a7a3b1970119a1a777
Alex Hebing <a.h.j.hebing@uu.nl>
2019-06-11 15:01:40 +0200

    #45. Upgrade the look of things

--- frontend/src/panel-ld-item/ld-item-view.ts
+++ frontend/src/panel-ld-item/ld-item-view.ts
@@ -10,6 +10,8 @@ import LabelView from '../utilities/label-view';
 import { JsonLdObject } from '../jsonld/json';
 import { triggerAsyncId } from 'async_hooks';
 
+import * as bulmaAccordion from 'bulma-accordion';
+
 export default class LdItemView extends View<Node> {
 
     properties: any = new Object();
@@ -37,7 +39,22 @@ export default class LdItemView extends View<Node> {
         let lblView = new LabelView({model: this.model}, false);
         lblView.render().$el.appendTo(this.$('#lblViewWrapper'));
 
+        this.prepareAccordions();
+
         return this;
+    }
+
+    /**
+     * Activate javascript for accordion control. Part of bulma-accordion package.
+     * @return {bulmaAccordion[]} Array of accordions.
+    */
+    prepareAccordions(): bulmaAccordion[] {
+        var accordions = [];
+        this.$('.accordions').each(function (i, element) {
+            accordions.push(new bulmaAccordion(element));
+        });
+
+        return accordions;
     }
 
     setItemProperties(): void {
@@ -47,15 +64,15 @@ export default class LdItemView extends View<Node> {
             }
 
             // iterate over the value for this attribute, which is an array of objects
-            // that either contain a value (i.e. string or date or whatever) and look like
+            // that either contain a value (i.e. string or date or whatever) and looks like
             //'{
             //    "@type": "http://www.w3.org/2001/XMLSchema#dateTime",
             //    "@value": "2085-12-31T04:33:15+0100"
             // }'
             // or that represents a link to another item, which looks like
-            // '{
+            //'{
             //    '@id': "http://www.wikidata.org/entity/Q331656"
-            //}
+            // }'
             for (let index in this.model.get(attribute)) {
 
                 let obj = this.model.attributes[attribute][index];
