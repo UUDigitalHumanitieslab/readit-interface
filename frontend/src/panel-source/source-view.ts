90373b9c110f051fe37182d5e8bd5e8b76180d67
Alex Hebing <a.h.j.hebing@uu.nl>
2019-06-21 11:18:13 +0200

    Refer #34. Implement HighlightabletextView into SourceView

--- frontend/src/panel-source/source-view.ts
+++ frontend/src/panel-source/source-view.ts
@@ -1,15 +1,49 @@
+import { ViewOptions as BaseOpt } from 'backbone';
 import { extend } from 'lodash';
-import View from '../core/view';
+import View from './../core/view';
+import Model from './../core/model';
 
 import sourceTemplate from './source-template';
 
-export default class SourceView extends View {
+import Node from './../jsonld/node';
 
-    render() {
+import mockSourceText from './../mock-data/mock-source-text';
+import HighlightableTextView from '../utilities/highlight/highlightable-text-view';
+import Graph from '../jsonld/graph';
+
+export interface ViewOptions extends BaseOpt<Model> {
+    highlight: Node;
+}
+
+export default class SourceView extends View {
+    highlight: Node;
+
+    constructor(options?: ViewOptions) {
+        super(options);
+
+        // TODO validate
+
+        if (options.highlight) {
+            this.highlight = options.highlight;
+        }
+    }
+
+    render(): this {
         this.$el.html(this.template(this));
 
         // TODO: if panel mode
         this.$el.addClass('explorer-panel');
+
+        let graph = new Graph([this.highlight]);
+
+        let htv = new HighlightableTextView({
+            text: mockSourceText,
+            collection: graph,
+            isEditable: true
+        });
+
+
+        this.$('highlightable-text-view').replaceWith(htv.render().$el);
 
         return this;
     }
