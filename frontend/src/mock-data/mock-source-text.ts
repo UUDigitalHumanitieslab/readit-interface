54f605a4758c7a7e4ba00118767c531a680dd3f2
Alex Hebing <a.h.j.hebing@uu.nl>
2019-06-21 11:17:03 +0200

    Refer #34. Mock source added

---
+++ frontend/src/mock-data/mock-source-text.ts
@@ -0,0 +1,387 @@
+var mockSourceText =
+`English descriptions of reading experiences
+<br><br>
+id_19
+Titre : The Idler in France / by the countess of Blessington Auteur :
+Blessington, Margaret Gardiner. Date d'édition : 1841. Droits : do-
+maine public. Provenance : Bibliothèque nationale de France.
+<br><br>
+id_19_a, p 40
+I remember reading years ago of the melancholy physiognomy of King
+Charles I, which when seen in his portrait by a Florentine sculptor, to
+whom it was sent in order that a bust should be made from it, drew forth
+the observation that the countenance indicated that its owner would come
+to a violent death.
+I was reminded of this anecdote by the face of the Duchesse d'Angoulême;
+for though I do not pretend to a prescience as to her future fate, I cannot
+help arguing from it that, even should a peaceful reign await her, the
+fearful trials of her youth have destroyed in her the power of enjoyment;
+and that on a throne she can never forget the father and mother she saw
+hurried from it, to meet every insult that malice could invent, or cruelty
+could devise, before a violent death freed them from their sufferings.
+<br><br>
+id_19_b, p 190
+I have been reading some French poems by Madame Amabel Tastu;
+and very beautiful they are. A sweet and healthy tone of mind breathes
+through them, and the pensiveness that characterises many of them,
+marks a reflecting spirit imbued with tenderness. There is great harmony,
+too, in the versification, as well as purity and elegance in the diction. How
+much some works make us wish to know their authors, and vice versa ! I
+feel, while reading her poems, that I should like Madame Amabel Tastu;
+while other books, whose cleverness I admit, convince me I should not
+like the writers.
+<br><br>
+id_19_c, p 295
+I walked with Comte d'O-- this evening into the Champs-Elysees,
+and great was the change effected there within the last few days. It looks
+ruined and desolate, the ground cut up by the pieces of cannon, and
+troops as well as the mobs that have made it a thoroughfare, and many
+of the trees greatly injured, if not destroyed.
+A crowd was assembled around a man who was reading aloud for their
+edification a proclamation nailed to one of the trees. We paused for a
+moment to hear it, when some of the persons recognising my companion,
+shouted aloud, "Vive le Comte d'Orsay! Vive le Comte d'Orsay" and the
+cry being taken up by the mass, the reader was deserted, the fickle mul-
+titude directing all their attention and enthusiasm to the new comer. We
+had some difficulty in escaping from these troublesome and unexpected
+demonstrations of good will; and, while hurrying from the scene of this
+impromptu ovation to the unsought popularity of my companion, I made
+him smile by hinting at the danger in which he stood of being raised to
+the vacant throne by those who seem not to know or care who is to till
+it.
+<br><br>
+id_20
+Old Bailey Proceedings Online (www.oldbaileyonline.org, version 8.0, 28
+February 2019), September 1766, trial of Thomas Jacocks , otherwise
+Gecox, otherwise Jecox (t17660903-71).
+<br><br>
+id_20_a
+Mr. Sharpling : "I am a clockmaker, and live in Duck-lane; I have been
+acquainted with the prisoner about two or three years; last Thursday was
+with him between four and five o'clock, he was very much in liquor, this
+was at the Bear and Ragged staff; I was looking over the newspaper, he
+insisted on my drinking a glass of wine with him; I said, you had better
+go and lie down; he said to me you shall drink a glass, and then you and
+I will go and take a ride for six or seven miles".
+<br><br>
+id_21
+Titre : Letters from France, written in the years 1803 and 1804 : in-
+cluding a particular account of Verdun, and the situation of the British
+captives in that city. Volume 2 / by James Forbes. Date d'édition : 1806.
+Droits : domaine public Provenance : Bibliothèque nationale de France.
+<br><br>
+id_21_a, p 295-296
+We left the Meuse at some distance on our right, but found a smaller
+river called the Aire, flowing through Varennes, and supplying the mills at
+the foot of the surrounding eminences. It is a most insignificant place, but
+its connexion with the great events of the revolution makes it interesting
+we were shown the dwelling-house where the royal family were arrested,
+and that of Sausse the tallow-chandler, the procureur de la commune,
+where they passed the night on the 21st of June 1791, as I mentioned
+at St. Menehoud. On viewing the spot, and reading the account of the
+arrest in different histories of the revolution, it is evident to me that
+matters must have been very ill managed either, by the ill-fated monarch
+or those about him.
+<br><br>
+id_21_b, p 438-439
+We have been much gratified for these ten days past with Texier's
+public reading, whom you have probably heard in London, where he was
+in vogue many years. His versatility of talent is very great, and French
+comedy is admirably adapted to display it. He has met with general
+encouragement at Verdun, and is now on bis route to several of the
+German courts ; from whence he intends to proceed to St. Petersburgh.
+<br><br>
+id_22
+Titre : A little Tour in France / by Henry James ; with 94 illustrations
+by Joseph Pennell Auteur : James, Henry (1843-1916). Date d'édition :
+1900 Contributeur : Pennell, Joseph (1857-1926). Illustrateur. Droits :
+domaine public. Provenance : Bibliothèque nationale de France.
+<br><br>
+id_22_a, p 177
+The whole place, with its repeated steps, its balustrades, its massive
+and plentiful stonework, is full of the air of the last century -- sent bien
+son dix-huitième siècle ; none the less so, I am afraid, that, as I read
+in my faithful Murray, after the revocation of the Edict of Nantes the
+block, the stake, the wheel had been erected here for the benefit of the
+desperate Camisards.
+<br><br>
+id_22_b, p 265
+"C'est l'heure de la lecture," remarked my guide and a group of con-
+valescents -- all the patients I saw were women -- were gathered in the
+centre around a nun, the points of whose white hood nodded a little
+above them and whose gentle voice came to us faintly, with a little echo,
+down the high perspective. I know not what the good sister was reading
+-- a dull book, I am afraid -- but there was so much colour and such
+a fine, rich air of tradition about the whole place that it seemed to me
+I would have risked listening to her. I turned away, however, with that
+sense of defeat which is always irritating to the appreciative tourist, and
+pottered about Beaune rather vaguely for the rest of my hour [...].
+<br><br>
+id_22_c, p 249-251
+I sat in the train (which waited a long time at Lyons) and, by the
+light of one of the big lamps on the platform, read all sorts of disagreeable
+things in certain radical newspapers which I had bought at the bookstall.
+I gathered from these sheets that Lyons was in extreme commotion.
+<br><br>
+id_23
+Titre : The Nocturnal pleasures of Paris. A guide to the gay city, by
+captain Wray Sylvester Auteur : Sylvester, Wray. Date d'édition : 1889.
+Droits : domaine public. Provenance : Bibliothèque nationale de France.
+<br><br>
+id_23_a, p9-10
+I remember reading in a smart London periodical, since extinct, an
+article, headed "The Judgment of Paris," which was evidently written
+by such a person, and in which the writer endeavoured to prove that
+the reputation of Paris, as the gayest city in the world, was a wholly
+undeserved one, and that her claim to this distinction rested on the
+falsest of false pretences. Now this is all nonsense.
+<br><br>
+id_23_b, p 126
+And now for a remark which is somewhat of a, personal nature.
+I daresay, some hypocrite will get hold of this book, and after reading it,
+cry: "What a blackguard that Captain Wray is to write about such
+things. How can he do so?" Hypocrite, I answer you by saying : " It is not I who advise you to
+frequent these places, or to seek such enjoyments. My role is to describe
+them to you, with strict truth, and to moralize neither one way nor the
+other.
+<br><br>
+Id_24
+The London venture : ELTec edition Arlen, Michael (pseud.) (1857-
+1924), ELTeC conversion Lou Burnard, Published as part of ELTeC
+September 2018, Source : The Project Gutenberg eBook of The London
+Venture, by Michael Arlen. The London ventureArlen, Michael (Dikran
+Kouyoumdjian) (1857-1924). London: W Heinemann 1920,
+https://distantreading.github.io/ELTeC/eng/ENG19201.html
+<br><br>
+Id_24_a
+I wondered if I had not done a very silly thing in being independent,
+and in not doing as my brothers had done, reading "The Times" in
+an office every morning from ten to twelve, and playing dominoes in
+the afternoon, and auction bridge in the evening, and having several
+thousands a year when I was forty, and a Wolseley car to take my wife
+for a holiday to Windermere, because she looked pale, or because we were
+bored with each other. I smiled to think of the look on my brothers' faces
+if I suddenly appeared at their ofice one morning, and said that it was
+no good, and that I couldn't write, and was very hungry.
+<br><br>
+Id_25
+Women in love : ELTeC edition, Lawrence, David Herbert (1885-1930),
+HTML version Col ChoatAl HainesMenno de Leeuw, ELTeC conversion
+Lou Burnard, Published as part of ELTeC, The Project Gutenberg EBook
+of Women in Love, by D. H. Lawrence Women in love New York: Thomas
+Seltzer 1920,
+https://distantreading.github.io/ELTeC/eng/ENG19200.html
+<br><br>
+Id_25_a
+The two men looked at the station clock, having nothing further to
+"What were you reading in the paper?" Birkin asked.
+Gerald looked at him quickly.
+"Isn't it funny, what they do put in the newspapers," he said.
+"Here are two leaders--" he held out his Daily Telegraph, "full of the
+ordinary newspaper cant--" he scanned the columns down--"and then
+there's this little--I dunno what you'd call it, essay, almost--appearing
+with the leaders, and saying there must arise a man who will give new
+values to things, give us new truths, a new attitude to life, or else we
+shall be a crumbling nothingness in a few years, a country in ruin--"
+"I suppose that's a bit of newspaper cant, as well," said Birkin.
+"It sounds as if the man meant it, and quite genuinely," said Gerald.
+"Give it to me," said Birkin, holding out his hand for the paper.
+The train came, and they went on board, sitting on either side a
+little table, by the window, in the restaurant car. Birkin glanced over his
+paper, then looked up at Gerald, who was waiting for him.
+"I believe the man means it," he said, "as far as he means anything."
+"And do you think it's true? Do you think we really want a new
+gospel?" asked Gerald. Birkin shrugged his shoulders.
+"I think the people who say they want a new religion are the last
+to accept anything new. They want novelty right enough. But to stare
+straight at this life that we've brought upon ourselves, and reject it,
+absolutely smash up the old idols of ourselves, that we sh'll never do.
+You've got very badly to want to get rid of the old, before anything new
+will appear--even in the self."
+Gerald watched him closely.
+"You think we ought to break up this life, just start and let fly?" he
+asked. "This life. Yes I do. We've got to bust it completely, or shrivel inside
+it, as in a tight skin. For it won't expand any more."
+<br><br>
+id_26
+PdE, Fauster, Le_Fauster, 3:31 PM, 22 févr. 2016, Twitter for Android
+<br><br>
+id_26_a
+Just read the all tweets I receive after Top8... waow guys ! I'm not
+sure I deserve all of it but I'm really happy you liked it =) #CWC next
+<br><br>
+id_27
+Cindy Sturdivant, @clsturdivant, 4 mars 2019.
+<br><br>
+id_27_a
+Nothing quite like the smell of a new book!!
+[picture of someone smelling a book]
+<br><br>
+id_28
+Patricia, @PattMeira, 2 mars 2019
+<br><br>
+id_28_a
+Do you like to smell the books ? Am I the only one that does that a
+few times while I am reading ?
+<br><br>
+id_29
+Sabie @SabieFCB, 4 mars 2019
+<br><br>
+id_29_a
+This jewel by @say_shannon arrived in Belgium today - the cover is
+even more beautiful in real life - nothing better than the smell of a new
+book opened for the first time @BloomsburyBooks #Queendom
+<br><br>
+id_30
+Titre : The sketch book (4e éd.) / Washington Irving ; texte annoté
+par M. l'abbé A. Julien Auteur : Irving, Washington (1783-1859). Date
+d'édition : 1911. Droits : domaine public. Provenance : Bibliothèque
+nationale de France.
+<br><br>
+id_30_a
+There was something extremely agreeable in the cheerful flow of an-
+imal spirits of the little man ; and I confess I had heen somewhat sur-
+prised at his apt quotations from authors who certainly were not in the
+range of every-day reading. I mentioned this last circumstance to Frank
+Bracebridge, who told me with a smile that Master Simon's whole stock
+of erudition was confined to some half a dozen old authors, which the
+squire had put into his hands, and which he read over and over whenever
+he had a studious fit, as he sometimes had on rainy day, or a long winter
+evening.
+<br><br>
+id_31
+Freya Stark, The Coast of Incense: Autobiography 1933-1939, (London,
+1953), p. 198,
+http://www.open.ac.uk/Arts/reading/UK/record_details.php?id=34004, ac-
+cessed: 07 March 2019
+<br><br>
+id_31_a
+I have read the whole of Jane Austen and think of beginning over
+again. What a perfect woman - not only a writer - and what a sham she
+makes all this female emancipation seem! Nothing that is not genuine
+can stand this primitive severity.
+But Jane would have been quite at
+home and talked tea- table gossip with the ladies of Huraidha.
+<br><br>
+id_32
+Sandra Newnham, interview,
+https://www.roehampton.ac.uk/research-centres/memories-of-ction/archive/sandra,
+7'50.
+<br><br>
+id_32_a
+Well, there were no pictures in it... I can't... Well... books, they
+all looked the same because they were usually hard backs... they had a
+detachable flyleaf on them... hmmm... no... The Magic Faraway Tree
+was a plain green almost cloth kind of cover, so, it wasn't a library book,
+so... but it wasn't a new book, so... somebody else had given it to us, I
+guess...
+<br><br>
+id_33
+European Parlement, Debates, Thursday, 17 January 2008 - Strasbourg.
+http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-//EP//TEXT+CRE+20080117+ITE
+<br><br>
+id_33_a
+These were girls from the so-called Polish Service organisation. We
+saw them shouldering pickaxes and spades and marching joyfully off to
+work. There were also women tending looms and lathes. Times have
+changed. We now see women working in design offices, on computers, or
+on the new production lines for electronic equipment, mobile telephones
+and televisions. We also see the tired faces of women hurrying home after
+many hours of hard work to undertake other daily tasks. Those were the
+images that came to my mind as I read Mrs Figueiredo's report on the
+role of women in industry.
+For many years now, efforts have been made to protect women, reduce
+their workload and treat them in the same way as men are treated.
+Unfortunately, differences persist to this day.
+<br><br>
+id_34
+Elsie Brownlee, Elsie was born on the 24th June 1925 and died on the
+31st January 2015. Elsie is being interviewed by Mary Grover on the
+21st February 2013.
+https://www.readingsheffield.co.uk/readers-stories-2/elsie-brownlee/
+<br><br>
+id_34_a
+EB: No I can't remember that. I weren't a great one for rereading
+books. I never have been. That's it, I'm satisfied. If I read it again I
+can get a bit bored. I think 'Oh, I've read all this before'. So no, I don't
+remember keeping reading the same thing.
+<br><br>
+id_35
+Alma, Alma was born in 1928, she is being interviewd by Liz Hawkins.
+https://www.readingsheffield.co.uk/readers-stories-2/alma/
+<br><br>
+id_35_a
+A: No, at college, yes I think I was on the courses -- yes. I did read
+a book which made a big impression on me and it was called I Bought
+a Mountain and it was by Esmé and Thomas Somebody and I can't
+remember who they were. And this couple had bought a farm in Wales
+and, you know, set about it and made it work, and it was a sheep farm,
+of course. It was lovely. Years later, after I was married and we did a lot
+of camping, we actually went to camp on their grounds and I met Esmé
+and Thomas, and told them that as a teenager I had read their book.
+And by that time they were, you know, quite grizzled people.
+Yes, it
+was, that was fantastic. So that was a book that really made a . . .
+<br><br>
+id_35_b
+A: Not particularly, no. But if I did read a book that I liked, I did
+like it. It was another thing. I read Dylan Thomas' Under Milk Wood
+at that point and I can remember, again, looking for the characters as I
+walked about and 'that one would be that one, and that one would be
+that one'.
+<br><br>
+id_36
+Margaret C, Margaret was born on 17th April 1934. Margaret C is being
+interviewed by Loveday Herridge on the 23rd February 2013.
+https://www.readingsheffield.co.uk/readers-stories-2/margaret-c/
+<br><br>
+id_36_a
+MC: Maybe it was just an hour. There was very high to the ceiling
+-- books in stacks -- and you would take a chair down there and sit and
+read but there were distractions and I never really enjoyed that. I found
+I'd read a couple of pages and then there was a distraction.
+<br><br>
+id_37
+Barbara Green This interview was transcribed by Sue Roe in January
+2018, Barbara was born in Pitsmoor [in] 1944. She is being interviewed
+by Mary Grover on 27 July 2017, assisted by Sue Roe,
+https://www.readingsheffield.co.uk/readers-stories-2/barbara-green/
+<br><br>
+id_37_a
+SR: I've just remembered something.
+Did anybody ever make you
+feel reading was a guilty pleasure? That you shouldn't be doing it.
+BG: No, I can't say that was the case cos I think Mum was quite
+happy as long as I was quiet.
+[general laughter] I'm painting a very
+dark picture of my Mum. She was a delightful person but she was quite
+happy for me to do my own thing and in those days, it was reading. Cos
+obviously if you were listening to radio you were impinging on somebody
+else's space, weren't you so? Unless you were listening as a family -- you
+probably weren't listening.
+<br><br>
+id_38
+Noel H. Noel was born on the 26th December 1939 in Wadsley, Sheeld,
+and died 2015. He is being interviewed by Loveday Herridge on the 1st
+August 2012.
+https://www.readingsheffield.co.uk/readers-stories-2/noel-h/
+<br><br>
+id_38_a
+LH: I'm not at all suggesting that literature should be the only thing
+that we talk about. Do you recall having encyclopaedias at home? Very
+often families had sets of encyclopaedias, didn't they, and often they were
+...
+NH: I think we had a general encyclopaedia. We had a bookcase.
+LH: Yes, yes.
+NH: And certainly . . . Oh, and I read . . . I do remember one Dickens
+book I read was Tale of Two Cities. I also remember being given a very
+early edition of Dombey and Son by one of my . . .
+when I stayed with my grandmother who was a widow in Wadsley Woodhouse when my
+grandfather had died many years before. A lady who lived in one of the
+cottages in the block where my grandmother was living gave me a very
+old edition of Dombey and Son, I mean old. I don't think it was a first
+edition. And I got through, I think, the first chapter.
+LH: And what did you do with the book?
+NH: I kept it for a long time and then it was disposed of.`;
+
+export default mockSourceText;
