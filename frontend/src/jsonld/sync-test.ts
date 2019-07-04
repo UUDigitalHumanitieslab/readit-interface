87472eeca87f85f6493f1a47d84d784379649bdc
Alex Hebing <a.h.j.hebing@uu.nl>
2019-07-04 14:20:31 +0200

    Refer #96. Basic test setup

---
+++ frontend/src/jsonld/sync-test.ts
@@ -0,0 +1,61 @@
+require('jasmine-ajax')
+
+import syncLD from './sync';
+import expandedData from './../mock-data/mock-expanded';
+import compactData from './../mock-data/mock-compact';
+import { webAnnoExerpt, contextIRI } from '../mock-data/mock-context';
+import Graph from './graph';
+import Node from './node';
+
+describe("sync", async function () {
+    let request, expandedGraph;
+    let success, error;
+
+    beforeEach(function () {
+        jasmine.Ajax.install();
+        success = jasmine.createSpy('success');
+        error = jasmine.createSpy('error');
+
+        expandedGraph = new Graph(expandedData);
+    });
+
+
+    afterEach(async function () {
+        jasmine.Ajax.uninstall();
+    });
+
+    it("does something", async function () {
+        let result = syncLD('POST', expandedGraph, { success, error, url: 'https://bogus.blah.foo', });
+
+        // wachten
+        await sleep(10);
+
+        request = jasmine.Ajax.requests.mostRecent();
+
+        console.log(request);
+
+
+        let response = {
+            "status": 200,
+            "contentType": "application/ld+json",
+            "responseText": JSON.stringify(compactData)
+        }
+        request.respondWith(response);
+
+        // assert something about request
+
+        await sleep(10);
+
+        expect(success).toHaveBeenCalled();
+
+        // response
+        let actual = await result;
+        console.log(actual);
+    });
+
+
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+})
