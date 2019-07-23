fdea9734d5f45cc80234a1b8938c1756e34efa4d
Julian Gonggrijp <dev@juliangonggrijp.com>
2019-07-23 13:16:58 +0200

    Add missing commas (#91)

--- frontend/src/jsonld/conversion.ts
+++ frontend/src/jsonld/conversion.ts
@@ -25,10 +25,10 @@ const knownConversions = {
     [xsd.integer]: {
         toLD(int: number): FlatTypedLiteral {
             return { '@value': int };
-        }
+        },
         fromLD(value: FlatTypedLiteral): number {
             return +value['@value'];
-        }
+        },
     },
     [xsd.nonNegativeInteger]: {
         toLD(int: number): FlatTypedLiteral {
@@ -36,36 +36,36 @@ const knownConversions = {
                 '@value': int,
                 '@type': xsd.nonNegativeInteger,
             };
-        }
+        },
         fromLD(value: FlatTypedLiteral): number {
             let obj = new Object(+value['@value']);
             obj['@type'] = xsd.nonNegativeInteger;
             return obj;
-        }
+        },
     },
     [xsd.double]: {
         toLD(double: number): FlatTypedLiteral {
             return { '@value': double };
-        }
+        },
         fromLD(value: FlatTypedLiteral): number {
             return +value['@value'];
-        }
+        },
     },
     [xsd.boolean]: {
         toLD(bool: boolean): FlatTypedLiteral {
             return { '@value': bool };
-        }
+        },
         fromLD(value: FlatTypedLiteral): boolean {
             return !!value['@value'];
-        }
+        },
     },
     [xsd.string]: {
         toLD(str: string): FlatLiteral {
             return { '@value': str };
-        }
+        },
         fromLD(value: FlatLiteral): string {
             return value['@value'].toString();
-        }
+        },
     },
     [xsd.dateTime]: {
         toLD(date: Date): FlatTypedLiteral {
@@ -73,10 +73,10 @@ const knownConversions = {
                 '@value': date.toJSON(),
                 '@type': xsd.dateTime,
             };
-        }
+        },
         fromLD(value: FlatTypedLiteral): Date {
             return new Date(value['@value']);
-        }
+        },
     },
 };
 
