c87ec129734d6fda717092e3d13eb9061d2e5fda
Alex Hebing <a.h.j.hebing@uu.nl>
2019-08-29 14:07:25 +0200

    Refer #116. Add return types and some casting to make Typescript happy

--- frontend/src/utilities/annotation-utilities.ts
+++ frontend/src/utilities/annotation-utilities.ts
@@ -17,7 +17,9 @@ export type AnnotationPositionDetails = {
  * Get the annotation's position details (i.e. node and character indices).
  * @param annotation The node to extract the details from.
  */
-export function getPositionDetails(annotation: Node) {
+export function getPositionDetails(annotation: Node): AnnotationPositionDetails {
+    validateType(annotation);
+
     let startSelector = getStartSelector(annotation);
     let endSelector = getEndSelector(annotation);
     return {
@@ -29,23 +31,11 @@ export function getPositionDetails(annotation: Node) {
 }
 
 /**
- * Get an array of items linked to the annotation. Will not include the annotation.
- * These items should all be deleted when an oa:Annotation is removed from a Graph.
+ * Get an array of items linked to the annotation. Will not include the annotation itself.
+ * These items should all included when deleting or adding the annotation from or to a Graph.
  */
-export function getLinkedItemsForDeletion(annotation: Node) {
-    return [
-        getSpecificResource(annotation),
-        getSelector(annotation),
-        getStartSelector(annotation),
-        getEndSelector(annotation)
-    ]
-}
-
-/**
- * Get an array of items linked to the annotation. Will not include the annotation.
- * These items should all be added when an oa:Annotation is added to a Graph.
- */
-export function getLinkedItemsForAddition(annotation: Node) {
+export function getLinkedItems(annotation: Node): Node[] {
+    validateType(annotation);
     return [
         getSpecificResource(annotation),
         getSelector(annotation),
@@ -59,6 +49,7 @@ export function getLinkedItemsForAddition(annotation: Node) {
  * Returns be null if a value cannot be found.
  */
 export function getCssClassName(annotation: Node): string {
+    validateType(annotation);
     let ontologyReferences = getOntologyReferencesFromBody(annotation);
     if (ontologyReferences.length > 1) {
         throw RangeError('An oa:Annotation cannot be associated with more than one ontology item');
@@ -69,48 +60,38 @@ export function getCssClassName(annotation: Node): string {
 }
 
 /**
- * Get Body associated with this annotation
- */
-export function getBody(annotation: Node): Node[] {
-    return <Node[]>annotation.get(oa.hasBody);
-}
-
-/**
  * Get the SpecificResource associated with this annotation.
  */
-export function getSpecificResource(annotation: Node) {
-    return annotation.get(oa.hasTarget)[0];
+export function getSpecificResource(annotation: Node): Node {
+    validateType(annotation);
+    return <Node>annotation.get(oa.hasTarget)[0];
 }
 
 /**
 * Get the Selector associated with this annotation.
  */
-export function getSelector(annotation: Node) {
+export function getSelector(annotation: Node): Node {
+    validateType(annotation);
     let specificResource = getSpecificResource(annotation);
-    return specificResource.get(oa.hasSelector)[0];
+    return <Node>specificResource.get(oa.hasSelector)[0];
 }
 
 /**
  * Get the StartSelector associated with this annotation.
  */
-export function getStartSelector(annotation: Node) {
+export function getStartSelector(annotation: Node): Node {
+    validateType(annotation);
     let selector = getSelector(annotation);
-    return selector.get(oa.hasStartSelector)[0];
+    return <Node>selector.get(oa.hasStartSelector)[0];
 }
 
 /**
  * Get the EndSelector associated with this annotation.
  */
-export function getEndSelector(annotation: Node) {
+export function getEndSelector(annotation: Node): Node {
+    validateType(annotation);
     let selector = getSelector(annotation);
-    return selector.get(oa.hasEndSelector)[0];
-}
-
-/**
- * Verify that the node is an instance of oa:Annotation.
- */
-export function isAnnotationInstance(annotation: Node) {
-    return isType(annotation, oa.Annotation);
+    return <Node>selector.get(oa.hasEndSelector)[0];
 }
 
 /**
@@ -132,17 +113,27 @@ export function isCompleteAnnotation(annotation: Node): boolean {
         throw new TypeError(getErrorMessage("Selector", selector, "vocab('RangeSelector')"));
     }
 
-    let startSelector = selector.get(oa.hasStartSelector)[0];
+    let startSelector = getStartSelector(annotation);
     if (!startSelector || !isType(startSelector, oa.XPathSelector)) {
         throw new TypeError(getErrorMessage('StartSelector', startSelector, 'oa:XPathSelector'));
     }
 
-    let endSelector = selector.get(oa.hasEndSelector)[0];
+    let endSelector = getEndSelector(annotation);
     if (!endSelector || !isType(endSelector, oa.XPathSelector)) {
         throw new TypeError(getErrorMessage('Endselector', endSelector, 'oa:XPathSelector'));
     }
 
     return true;
+}
+
+/**
+ * Verify that the node is an instance of oa:Annotation.
+ */
+function validateType(annotation: Node): void {
+    if (!isType(annotation, oa.Annotation)) {
+        throw new TypeError(
+            `Node ${annotation.get('@id')} is not an instance of oa:Annotation`);
+    }
 }
 
 /**
@@ -155,7 +146,7 @@ function getOntologyReferencesFromBody(annotation: Node): Node[] {
 /**
  * Helper function for isCompleteAnnotation that constructs error message from variable parts.
  */
-function getErrorMessage(itemName: string, item?: Node, expectedType?: string) {
+function getErrorMessage(itemName: string, item?: Node, expectedType?: string): string {
     if (item) {
         return `${itemName} with id '${item.get('@id')}' should be of type ${expectedType}`;
     }
@@ -169,7 +160,7 @@ function getErrorMessage(itemName: string, item?: Node, expectedType?: string) {
  * @param selector XPathSelector with a rdf:Value like 'substring(.//*[${nodeIndex}]/text(),${characterIndex})'
  */
 function getNodeIndex(selector: Node): number {
-    let xpath = selector.get(rdf.value)[0];
+    let xpath = <string>selector.get(rdf.value)[0];
     let index = xpath.indexOf('[') + 1;
     let endIndex = xpath.indexOf(']');
     return +xpath.substring(index, endIndex);
@@ -179,9 +170,9 @@ function getNodeIndex(selector: Node): number {
  * Get the character index from an XPathSelector
  * @param selector XPathSelector with a rdf:Value like 'substring(.//*[${nodeIndex}]/text(),${characterIndex})'
  */
-function getCharacterIndex(selector: Node): any {
-    let xpath = selector.get(rdf.value)[0];
+function getCharacterIndex(selector: Node): number {
+    let xpath = <string>selector.get(rdf.value)[0];
     let startIndex = xpath.indexOf(',') + 1;
     let endIndex = xpath.length - 1;
-    return xpath.substring(startIndex, endIndex);
+    return +xpath.substring(startIndex, endIndex);
 }
