0c2e4aa8d7179344fc7eb1a4c36f8f13b1e1af10
Alex Hebing <a.h.j.hebing@uu.nl>
2019-08-29 14:35:54 +0200

    Refer #116. Documentation added in utilities

--- frontend/src/utilities/utilities.ts
+++ frontend/src/utilities/utilities.ts
@@ -5,11 +5,18 @@ import { skos, rdfs } from './../jsonld/ns';
 
 export const labelKeys = [skos.prefLabel, rdfs.label, skos.altLabel];
 
+/**
+ * Get a label from the node.
+ */
 export function getLabel(node: Node): string {
     let labelKey = find(labelKeys, key => node.has(key));
     if (labelKey) return node.get(labelKey)[0] as string;
 }
 
+/**
+ * Create a css class name based on the node's label.
+ * Returns null if no label is found.
+ */
 export function getCssClassName(node: Node): string {
     let label = getLabel(node);
 
