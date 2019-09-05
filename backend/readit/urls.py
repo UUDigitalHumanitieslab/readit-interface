UUDigitalHumanitieslab/readit-interface

commit 12d14feed89dcae8a470a9927c3c0b50b351cacd

Upgrade the backend while it's still easy to do
@alexhebing you need to run these commands when you cross this commit:

    yarn back pip-sync
    yarn django migrate

backend/readit/urls.py
@@ -1,19 +1,19 @@
 """readit URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 
 from rest_framework import routers
@@ -23,12 +23,12 @@ from .index import index
 api_router = routers.DefaultRouter()  # register viewsets with this router
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(api_router.urls)),
-    url(r'^api-auth/', include(
+    path('admin/', admin.site.urls),
+    path('api/', include(api_router.urls)),
+    path('api-auth/', include(
         'rest_framework.urls',
         namespace='rest_framework',
     )),
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'', index),  # catch-all; unknown paths to be handled by a SPA
+    path('rest-auth/', include('rest_auth.urls')),
+    path('', index),  # catch-all; unknown paths to be handled by a SPA
 ]

backend/manage.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readit.settings")
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'readit.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()

backend/readit/settings.py
@@ -1,13 +1,13 @@
 """
 Django settings for readit project.
 
-Generated by 'django-admin startproject' using Django 1.11.16.
+Generated by 'django-admin startproject' using Django 2.2.5.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/topics/settings/
+https://docs.djangoproject.com/en/2.2/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.11/ref/settings/
+https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
@@ -17,10 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kxreeb3bds$oibo7ex#f3bi5r+d(1x5zljo-#ms=i2%ih-!pvn'
+SECRET_KEY = 'll9*4+3uzfl2@_(_%+j8gltmqd2!w^3fxrf7oiohv2vifl+da5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'readit.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -90,7 +90,7 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -123,7 +123,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 

backend/readit/wsgi.py
@@ -4,13 +4,13 @@ WSGI config for readit project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readit.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'readit.settings')
 
 application = get_wsgi_application()

backend/requirements.in
@@ -1,4 +1,4 @@
-django>=1.11,<2
+django>=2,<3
 djangorestframework
 django-rest-auth
 django-livereload-server

commit ade5b8132945a5b09f289e8bbb7d0d4264f400f3

Repare the backend index fallback
I accidentally broke this in 12d14fe.

backend/readit/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.contrib import admin
 
 from rest_framework import routers
@@ -30,5 +30,5 @@ urlpatterns = [
         namespace='rest_framework',
     )),
     path('rest-auth/', include('rest_auth.urls')),
-    path('', index),  # catch-all; unknown paths to be handled by a SPA
+    re_path(r'', index),  # catch-all; unknown paths to be handled by a SPA
 ]