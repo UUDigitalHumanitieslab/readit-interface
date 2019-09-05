01174ba9af339cd17e06c5078a372fb6a31de439
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-09-05 15:08:59 +0200

    Define the graph of the vocab app (#83)

---
+++ backend/vocab/constants.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+VOCAB_INFIX = 'vocab/'
+VOCAB_NS = settings.RDF_NAMESPACE_ROOT + VOCAB_INFIX
---
+++ backend/vocab/graph.py
@@ -0,0 +1,5 @@
+from rdflib_django.utils import get_named_graph
+
+from .constants import VOCAB_NS
+
+vocab = get_named_graph(VOCAB_NS)
