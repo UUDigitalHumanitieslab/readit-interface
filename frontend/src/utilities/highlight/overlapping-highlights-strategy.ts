4477c033379bf282db82d1640d41f6c9985b4e0d
Alex Hebing <a.h.j.hebing@uu.nl>
2019-09-10 14:46:40 +0200

    Implement getOverlaps() (#103)

--- frontend/src/utilities/highlight/overlapping-highlights-strategy.ts
+++ frontend/src/utilities/highlight/overlapping-highlights-strategy.ts
@@ -1,4 +1,5 @@
 
+import { orderBy } from 'lodash';
 import { AnnotationPositionDetails } from './../annotation-utilities';
 import HighlightView from './highlight-view';
 
@@ -9,24 +10,79 @@ export type HighlightIndex = {
     isStart: boolean
 }
 
+export type OverlappingHighlights = {
+    highlightViews: HighlightView[];
+    positionDetails: AnnotationPositionDetails;
+}
+
 export default class OverlappingHighlightsStrategy {
+    getOverlaps(highlightViews: HighlightView[]): OverlappingHighlights[] {
+        let results = [];
+
+        let currentlyActive = 0;
+        let currentOverlapHighlights = [];
+
+        let currentOverlapStartNodeIndex: number;
+        let currentOverlapStartCharacterIndex: number;
+        let currentOverlapEndNodeIndex: number;
+        let currentOverlapEndCharacterIndex: number;
+
+        let highlightIndices = this.getHighlightIndices(highlightViews);
+        let orderedIndices = orderBy(highlightIndices, ['nodeIndex', 'characterIndex', 'isStart'], ['asc', 'asc', 'desc']);
+
+        orderedIndices.forEach(index => {
+            if (index.isStart) {
+                currentlyActive++;
+                currentOverlapHighlights.push(index.instance);
+
+                if (currentlyActive === 2) {
+                    currentOverlapStartNodeIndex = index.nodeIndex;
+                    currentOverlapStartCharacterIndex = index.characterIndex;
+                }
+            }
+            else {
+                currentlyActive--;
+
+                if (currentlyActive === 1) {
+                    currentOverlapEndNodeIndex = index.nodeIndex;
+                    currentOverlapEndCharacterIndex = index.characterIndex;
+
+                    results.push({
+                        highlightViews: currentOverlapHighlights, positionDetails: {
+                            startNodeIndex: currentOverlapStartNodeIndex,
+                            startCharacterIndex: currentOverlapStartCharacterIndex,
+                            endNodeIndex: currentOverlapEndNodeIndex,
+                            endCharacterIndex: currentOverlapEndCharacterIndex
+                        }
+                    });
+                    currentOverlapHighlights = [];
+                }
+
+                if (currentlyActive === 0) {
+                    currentOverlapHighlights = [];
+                }
+            }
+        });
+
+        return results;
+    }
 
     getHighlightIndices(highlightViews: HighlightView[]): HighlightIndex[] {
         let indices = [];
 
         highlightViews.forEach(hV => {
             indices.push({
-               instance: hV,
-               nodeIndex: hV.positionDetails.startNodeIndex,
-               characterIndex: hV.positionDetails.startCharacterIndex,
-               isStart: true
+                instance: hV,
+                nodeIndex: hV.positionDetails.startNodeIndex,
+                characterIndex: hV.positionDetails.startCharacterIndex,
+                isStart: true
             });
 
             indices.push({
-               instance: hV,
-               nodeIndex: hV.positionDetails.endNodeIndex,
-               characterIndex: hV.positionDetails.endCharacterIndex,
-               isStart: false
+                instance: hV,
+                nodeIndex: hV.positionDetails.endNodeIndex,
+                characterIndex: hV.positionDetails.endCharacterIndex,
+                isStart: false
             });
         });
 
