a083adb049857357c203a7abe9b2b34757a57b45
Alex Hebing <a.h.j.hebing@uu.nl>
2019-09-11 08:48:19 +0200

    Initial version of overlappingHighlightsView (#103)

---
+++ frontend/src/utilities/highlight/overlapping-highlights-view.ts
@@ -0,0 +1,77 @@
+import { ViewOptions as BaseOpt } from 'backbone';
+import { extend } from 'lodash';
+
+import View from '../../core/view';
+import Node from '../../jsonld/node';
+
+
+import { AnnotationPositionDetails } from '../annotation-utilities';
+import HighlightView from './highlight-view';
+
+import OverlappingHighlightsTemplate from './overlapping-highlights-template'
+
+
+export interface ViewOptions extends BaseOpt<Node> {
+    /**
+     * Range object containing the ClientDomRects that the overlap view should be based on.
+     */
+    range: Range;
+
+    /**
+     * Position details of the highlight.
+     */
+    positionDetails: AnnotationPositionDetails;
+
+     /**
+     * The first positioned parent, i.e. with position relative, absolute or fixed,
+     * relative to which the highlight will be positioned.
+     */
+    relativeParent: JQuery<HTMLElement>;
+}
+
+export default class OverlappingHighlightsView extends View {
+    hV: HighlightView;
+
+    constructor(options: ViewOptions) {
+        if (!options.range) throw TypeError("range cannot be null or empty");
+        if (!options.positionDetails) throw TypeError("positionDetails cannot be null or undefined");
+        if (!options.relativeParent) throw TypeError("relativeParent cannot be null or empty");
+
+        super(options);
+
+        this.hV = new HighlightView({
+            model: undefined,
+            cssClass: 'is-overlap',
+            range: options.range,
+            positionDetails: options.positionDetails,
+            relativeParent: options.relativeParent,
+            isDeletable: false
+        });
+    }
+
+
+    render(): this {
+        this.$el.detach();
+        this.hV.render().$el.appendTo(this.$el);
+        return this;
+    }
+
+    getHeight(): number {
+        return this.hV.getHeight();
+    }
+
+    getTop(): number {
+        return this.hV.getTop();
+    }
+
+    getPositionDetails(): AnnotationPositionDetails{
+        return this.hV.getPositionDetails();
+    }
+}
+extend(OverlappingHighlightsView.prototype, {
+    tagName: 'div',
+    className: 'overlappping-highlight',
+    template: OverlappingHighlightsTemplate,
+    events: {
+    }
+});
