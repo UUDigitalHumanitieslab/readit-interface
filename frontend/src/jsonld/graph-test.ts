df17e1e58746ba27933d1acd0d513de4823f4636
Alex Hebing <a.h.j.hebing@uu.nl>
2019-09-12 08:43:47 +0200

    Fix Graph tests after adding mock items

--- frontend/src/jsonld/graph-test.ts
+++ frontend/src/jsonld/graph-test.ts
@@ -15,24 +15,24 @@ describe('Graph', function() {
         });
 
         it('pass function iteratees unmodified', function() {
-            expect(graph.filter(model => model.has(oa.hasBody)).length).toBe(3);
+            expect(graph.filter(model => model.has(oa.hasBody)).length).toBe(5);
         });
 
         it('pass string iteratees to our JSON-LD aware wrapper', function() {
-            expect(graph.filter(oa.hasBody).length).toBe(3);
+            expect(graph.filter(oa.hasBody).length).toBe(5);
         });
 
         it('pass hash iteratees to our JSON-LD aware wrapper', function() {
             expect(graph.filter({
                 [oa.motivatedBy]: { '@id': oa.tagging },
-            }).length).toBe(3);
+            }).length).toBe(5);
             expect(graph.filter({
                 [oa.motivatedBy]: [{ '@id': oa.tagging }],
-            }).length).toBe(3);
+            }).length).toBe(5);
         });
 
         it('pass array iteratees to our JSON-LD aware wrapper', function() {
-            expect(graph.filter([oa.hasBody, oa.motivatedBy]).length).toBe(3);
+            expect(graph.filter([oa.hasBody, oa.motivatedBy]).length).toBe(5);
         });
 
         it('pass node instances to the underlying algorithm', function() {
