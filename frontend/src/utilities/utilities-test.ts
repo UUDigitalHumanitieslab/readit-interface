8f54555dcb6fb8679e6e42eeae19fa1fb4e00a29
Alex Hebing <a.h.j.hebing@uu.nl>
2019-09-12 13:14:08 +0200

    Wrap utilities test in describe('utilities')

--- frontend/src/utilities/utilities-test.ts
+++ frontend/src/utilities/utilities-test.ts
@@ -17,74 +17,75 @@ function getDefaultAttributes(): FlatLdObject {
             { '@value': 'Content' },
         ],
         [skos.altLabel]: [
-            { '@value': 'alternativeLabel'}
+            { '@value': 'alternativeLabel' }
         ],
     }
 }
 
-describe('utilities:getLabel', function() {
+describe('utilities', function () {
+    describe('getLabel', function () {
 
-    it('returns a label', function() {
-        let node = getDefaultNode();
-        expect(getLabel(node)).toBe('Content');
+        it('returns a label', function () {
+            let node = getDefaultNode();
+            expect(getLabel(node)).toBe('Content');
+        });
+
+        it('returns a preferred label before others', function () {
+            let node = getDefaultNode();
+            expect(getLabel(node)).toBe('Content');
+        });
+
+        it('returns alternative label if the preferred label is not present', function () {
+            let attributes = getDefaultAttributes();
+            delete attributes[skos.prefLabel];
+            let node = new Node(attributes);
+            expect(getLabel(node)).toBe('alternativeLabel');
+        });
     });
 
-    it('returns a preferred label before others', function() {
-        let node = getDefaultNode();
-        expect(getLabel(node)).toBe('Content');
+    describe('getCssClassName', function () {
+        it('returns a css class', function () {
+            let node = getDefaultNode();
+            expect(getCssClassName(node)).toBe('is-readit-content');
+        });
+
+        it('returns a lowercased css class stripped of spaces', function () {
+            let attributes = getDefaultAttributes();
+            attributes[skos.prefLabel] = [{ "@value": "A Capitalized Label With Spaces" }];
+            let node = new Node(attributes);
+            expect(getCssClassName(node)).toBe('is-readit-acapitalizedlabelwithspaces');
+        });
+
+        it('ignores nodes without a label', function () {
+            let attributes = getDefaultAttributes();
+            delete attributes[skos.prefLabel];
+            delete attributes[skos.altLabel];
+            let node = new Node(attributes);
+            expect(getCssClassName(node)).toBeNull();
+        });
     });
 
-    it('returns alternative label if the preferred label is not present', function() {
-        let attributes = getDefaultAttributes();
-        delete attributes[skos.prefLabel];
-        let node = new Node(attributes);
-        expect(getLabel(node)).toBe('alternativeLabel');
-    });
-});
+    describe('isRdfsClass', function () {
+        it('recognizes type rdfs:Class', function () {
+            let node = getDefaultNode();
+            expect(isRdfsClass(node)).toBe(true);
+        });
 
-describe('utilities:getCssClassName', function() {
+        it('recognizes type rdfs:subClassOf', function () {
+            let attributes = getDefaultAttributes();
+            attributes['@type'] = [rdfs('notClass')];
+            attributes[rdfs.subClassOf] = [{ '@id': 'anything' }]
+            let node = new Node(attributes);
 
-    it('returns a css class', function() {
-        let node = getDefaultNode();
-        expect(getCssClassName(node)).toBe('is-readit-content');
-    });
+            expect(isRdfsClass(node)).toBe(true);
+        });
 
-    it('returns a lowercased css class stripped of spaces', function() {
-        let attributes = getDefaultAttributes();
-        attributes[skos.prefLabel] = [{ "@value": "A Capitalized Label With Spaces" }];
-        let node = new Node(attributes);
-        expect(getCssClassName(node)).toBe('is-readit-acapitalizedlabelwithspaces');
-    });
+        it('ignores other types', function () {
+            let attributes = getDefaultAttributes();
+            attributes['@type'] = [rdf.Property];
+            let node = new Node(attributes);
 
-    it('ignores nodes without a label', function() {
-        let attributes = getDefaultAttributes();
-        delete attributes[skos.prefLabel];
-        delete attributes[skos.altLabel];
-        let node = new Node(attributes);
-        expect(getCssClassName(node)).toBeNull();
-    });
-});
-
-describe('utilities:isRdfsClass', function() {
-    it('recognizes type rdfs:Class', function() {
-        let node = getDefaultNode();
-        expect(isRdfsClass(node)).toBe(true);
-    });
-
-    it('recognizes type rdfs:subClassOf', function() {
-        let attributes = getDefaultAttributes();
-        attributes['@type'] = [ rdfs('notClass') ];
-        attributes[rdfs.subClassOf] = [{ '@id': 'anything'}]
-        let node = new Node(attributes);
-
-        expect(isRdfsClass(node)).toBe(true);
-    });
-
-    it('ignores other types', function() {
-        let attributes = getDefaultAttributes();
-        attributes['@type'] = [ rdf.Property ];
-        let node = new Node(attributes);
-
-        expect(isRdfsClass(node)).toBe(false);
+            expect(isRdfsClass(node)).toBe(false);
+        });
     });
 });
