61b216b8acc1045a16cd0e6e3512079ae8be6b0b
Julian Gonggrijp <dev@juliangonggrijp.com>
2019-09-20 18:56:44 +0200

    Give the terminal reporter better output with console.info

--- frontend/src/terminalReporter.ts
+++ frontend/src/terminalReporter.ts
@@ -1,8 +1,29 @@
 import * as Reporter from 'jasmine-terminal-reporter';
 
+const splitter = '\n';
+const buffer: string[] = [];
+
+function output(text: string): void {
+    if (text === '') return;
+    console.info(text);
+}
+
+function print(text: string): void {
+    if (text.includes(splitter)) {
+        const parts = text.split(splitter);
+        buffer.push(parts.shift());
+        output(buffer.join(''));
+        buffer.splice(0);
+        if (!text.endsWith(splitter)) buffer.push(parts.pop());
+        parts.forEach(output);
+    } else {
+        buffer.push(text);
+    }
+}
+
 const env = jasmine.getEnv();
 const reporter = new Reporter({
-    print: console.log,
+    print,
 });
 
 env.addReporter(reporter);
