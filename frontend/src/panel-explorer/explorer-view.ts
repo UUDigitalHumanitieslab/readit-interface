1b289e6010fa11b7915ff2c73bd361190aaa5919
Alex Hebing <a.h.j.hebing@uu.nl>
2019-09-20 10:16:24 +0200

    Add passing of ontology to explorerview.

--- frontend/src/panel-explorer/explorer-view.ts
+++ frontend/src/panel-explorer/explorer-view.ts
@@ -3,15 +3,19 @@ import { extend, sumBy, method, bind, debounce, findLast } from 'lodash';
 import Model from '../core/model';
 import View from '../core/view';
 
+import Graph from '../jsonld/graph';
 import PanelStackView from './explorer-panelstack-view';
 import EventController from './explorer-event-controller';
+
 
 export interface ViewOptions extends BaseOpt<Model> {
     // TODO: do we need a PanelBaseView?
     first: View;
+    ontology: Graph;
 }
 
 export default class ExplorerView extends View {
+    ontology: Graph;
     stacks: PanelStackView[];
 
     eventController: EventController;
@@ -30,10 +34,14 @@ export default class ExplorerView extends View {
 
     constructor(options?: ViewOptions) {
         super(options);
+        if (!options.ontology) throw new TypeError('ontology cannot be null or undefined');
+
+        this.ontology = options.ontology;
         this.stacks = [];
         this.eventController = new EventController(this);
         this.rltPanelStack = new Map();
         this.push(options.first);
+
 
         this.$el.on('scroll', debounce(bind(this.onScroll, this), 500));
     }
