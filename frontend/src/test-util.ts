88f31f957b88d18271fa52af7b5fc04b5ad45723
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-09-25 17:03:06 +0200

    Add a fixture for using the store in unittests (#119)

--- frontend/src/test-util.ts
+++ frontend/src/test-util.ts
@@ -1,7 +1,20 @@
 import '@dhl-uu/jquery-promise';
 
+import Store from './jsonld/store';
 import './global/hbsHelpers';
 import { i18nPromise } from './global/i18n';
+
+/**
+ * Wrap the following functions in beforeEach() and afterEach(),
+ * respectively, to use a store in all tests of a suite.
+ */
+export function startStore() {
+    this._globalGraph = new Store();
+}
+export function endStore() {
+    this._globalGraph.off().stopListening().stopReplying().reset();
+    delete this._globalGraph;
+}
 
 /**
  * Wrap this in a beforeAll() to enable i18n in all tests of a suite.
