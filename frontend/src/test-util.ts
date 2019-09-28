ac0cc3459788e7cba8167bef9e08f5f4d8a94c77
Julian Gonggrijp <dev@juliangonggrijp.com>
2019-09-28 15:52:25 +0200

    Add the introspector reporter in order to access the current name

--- frontend/src/test-util.ts
+++ frontend/src/test-util.ts
@@ -1,5 +1,8 @@
 import '@dhl-uu/jquery-promise';
 
+import { extend } from 'lodash';
+import { Events } from 'backbone';
+
 import './global/hbsHelpers';
 import { i18nPromise } from './global/i18n';
 
@@ -9,3 +12,35 @@ import { i18nPromise } from './global/i18n';
 export function enableI18n() {
     return i18nPromise;
 }
+
+/**
+ * A trick to get access to the current spec/suite.
+ */
+const introspector = {
+    spec: false,
+    names: [],
+    path() {
+        return this.names.join(' > ');
+    },
+    suiteStarted(info) {
+        this.names.push(info.description);
+        this.trigger('start:suite');
+    },
+    specStarted(info) {
+        this.spec = true;
+        this.names.push(info.description);
+        this.trigger('start:spec');
+    },
+    specDone() {
+        this.trigger('end:spec');
+        this.spec = false;
+        this.names.pop();
+    },
+    suiteDone() {
+        this.trigger('end:suite');
+        this.names.pop();
+    },
+};
+
+extend(introspector, Events);
+jasmine.getEnv().addReporter(introspector);
