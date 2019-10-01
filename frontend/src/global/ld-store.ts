a7a346927070034dd9575c0160bb2242fe7f87dd
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-10-01 12:43:40 +0200

    Process feedback from the review (#119, #126)

--- frontend/src/global/ld-store.ts
+++ frontend/src/global/ld-store.ts
@@ -16,14 +16,17 @@ const defaultGraphs = [
     'http://dublincore.org/2012/06/14/dcelements.ttl',
     // activitystreams appears to consist of only a context.
     // as(),
-    // Keep schema at the bottom, because it is large and we do not
-    // use much of it.
-    // The jsonld version has invalid syntax so we're using Turtle.
-    'https://schema.org/version/latest/schema.ttl',
+    // We do not prefetch schema.org, because it is large and we do
+    // not use much of it. Individual terms can be imported on demand.
 ];
 
 export const globalGraph = new Store();
 
 export function prefetch() {
-    defaultGraphs.forEach(ns => globalGraph.import(ns));
+    // For the time being, we skip the attempt to import directly,
+    // because most of our defaultGraphs don't support CORS and
+    // because it saves a bunch of error messages in the dev console.
+    // In the future, we may want to change this back into
+    // globalGraph.import(ns).
+    defaultGraphs.forEach(ns => globalGraph.importViaProxy(ns));
 }
