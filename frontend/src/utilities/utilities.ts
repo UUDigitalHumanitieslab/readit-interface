16adf25da5354dca05493032c29a39cbc16fc67d
Alex Hebing <a.h.j.hebing@uu.nl>
2019-10-02 11:40:11 +0200

    add readit('name') as valid item label

--- frontend/src/utilities/utilities.ts
+++ frontend/src/utilities/utilities.ts
@@ -1,9 +1,9 @@
 import { find, includes } from 'lodash'
 import Node from '../jsonld/node';
 
-import { skos, rdfs } from './../jsonld/ns';
+import { skos, rdfs, readit } from './../jsonld/ns';
 
-export const labelKeys = [skos.prefLabel, rdfs.label, skos.altLabel];
+export const labelKeys = [skos.prefLabel, rdfs.label, skos.altLabel, readit('name')];
 
 /**
  * Get a label from the node.
