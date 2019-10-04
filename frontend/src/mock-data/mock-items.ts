ac0e5d80ad07cbb6984d838686fc38b1b18e6a65
Alex Hebing <a.h.j.hebing@uu.nl>
2019-10-04 17:58:56 +0200

    Add one TextQuoteSelector in mock items

--- frontend/src/mock-data/mock-items.ts
+++ frontend/src/mock-data/mock-items.ts
@@ -95,11 +95,14 @@ export const anno1SpecificResource = {
     [oa.hasSelector]: [
         {
             "@id": item('400')
+        },
+        {
+            "@id": item('700')
         }
     ],
     [oa.hasSource]: [
         {
-            "@id": item('1')
+            "@id": source('1')
         }
     ]
 };
@@ -172,6 +175,44 @@ export const anno1EndSelector = {
         }
     ],
 }
+
+//a TextQuoteSelector belong to anno 1
+export const anno1TextQuoteSelector = {
+    "@id": item('700'),
+    "@type": [oa.TextQuoteSelector],
+    [oa.prefix]: [
+        {
+            '@value': `English descriptions of reading experiences
+            <br><br>
+            id_19
+            Titre : The Idler in France / by the countess of Blessington Auteur: `
+        }
+    ],
+    [oa.exact]: [
+        {
+            "@value": `Blessington, Margaret Gardiner. Date d'édition : 1841. Droits : do-
+            maine public. Provenance : Bibliothèque nationale de France.
+            <br><br>
+            id_19_a, p 40
+            I remember reading years ago of the melancholy physiognomy of King
+            Charles I, which when seen in his portrait by a Florentine sculptor, to
+            whom it was sent in order that a bust should be made from it, drew forth
+            the observation that the countenance indicated that its owner would come
+            to a violent death.
+            I was reminded of this anecdote by the face`
+        }
+    ],
+    [oa.suffix]: [
+        {
+            "@value": ` of the Duchesse d'Angoulême;
+            for though I do not pretend to a prescience as to her future fate, I cannot
+            help arguing`
+        }
+    ],
+}
+
+
+
 
 // annotation 2.
 export const anno2Instance = {
@@ -252,7 +293,7 @@ export const anno2SpecificResource = {
     ],
     [oa.hasSource]: [
         {
-            "@id": item('1')
+            "@id": source('1')
         }
     ]
 };
@@ -432,7 +473,7 @@ export const anno3SpecificResource = {
     ],
     [oa.hasSource]: [
         {
-            "@id": item('1')
+            "@id": source('1')
         }
     ]
 };
@@ -504,7 +545,6 @@ export const anno3EndSelector = {
         }
     ],
 }
-
 export default [
     anno1Instance,
     anno1ContentInstance,
@@ -512,6 +552,7 @@ export default [
     anno1RangeSelector,
     anno1StartSelector,
     anno1EndSelector,
+    anno1TextQuoteSelector,
     anno2Instance,
     anno2MediumInstance,
     anno2SpecificResource,
