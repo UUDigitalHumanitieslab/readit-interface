085c7091b3b67b7fb988f75aa39d994d46f4eb1f
Julian Gonggrijp <dev@juliangonggrijp.com>
2019-10-08 14:44:00 +0200

    Replace the ontology fixture prefix when needed (#82)

--- backend/ontology/fixture.py
+++ backend/ontology/fixture.py
@@ -9,6 +9,27 @@ from rdf.ns import *
 from . import namespace as my
 from .constants import SOURCE, SOURCE_FORMAT, SOURCE_PREFIX, ONTOLOGY_NS
 
+REPARSE_FORMAT = 'n3'
+
+
+def replace_prefix(graph_in, prefix_in, prefix_out):
+    """
+    Return a new graph in which SOURCE_PREFIX is replaced by ONTOLOGY_NS.
+
+    This function works by serializing `graph_in` to bytes, textually
+    replacing the prefix and then parsing the result into a new Graph.
+    Not efficient; do not use for large graphs.
+    """
+    if type(prefix_in) == str:
+        prefix_in = prefix_in.encode()
+    if type(prefix_out) == str:
+        prefix_out = prefix_out.encode()
+    serialized = graph_in.serialize(format=REPARSE_FORMAT)
+    replaced = serialized.replace(prefix_in, prefix_out)
+    graph_out = Graph()
+    graph_out.parse(data=replaced, format=REPARSE_FORMAT)
+    return graph_out
+
 
 def canonical_graph():
     """
@@ -19,6 +40,6 @@ def canonical_graph():
     """
     g = Graph()
     g.parse(SOURCE, format=SOURCE_FORMAT)
-    # if SOURCE_PREFIX != ONTOLOGY_NS:
-    #
+    if SOURCE_PREFIX != ONTOLOGY_NS:
+        return replace_prefix(g, SOURCE_PREFIX, ONTOLOGY_NS)
     return g
