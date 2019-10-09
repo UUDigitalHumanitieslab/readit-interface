5c850b9ba2cb191ea44df80cd33c0a51d8b45b2e
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-10-09 14:17:10 +0200

    Prefetch our own "fixed" namespaces (#82, #83, #85)

--- frontend/src/global/ld-store.ts
+++ frontend/src/global/ld-store.ts
@@ -3,6 +3,9 @@ import {
     rdfs,
     owl,
     as,
+    vocab,
+    staff,
+    readit,
 } from '../jsonld/ns';
 import Store from '../jsonld/store';
 
@@ -20,9 +23,16 @@ const defaultGraphs = [
     // not use much of it. Individual terms can be imported on demand.
 ];
 
+const inhouseGraphs = [
+    vocab(),
+    staff(),
+    readit(),
+];
+
 export const globalGraph = new Store();
 
 export function prefetch() {
+    inhouseGraphs.forEach(ns => globalGraph.import(ns));
     // For the time being, we skip the attempt to import directly,
     // because most of our defaultGraphs don't support CORS and
     // because it saves a bunch of error messages in the dev console.
