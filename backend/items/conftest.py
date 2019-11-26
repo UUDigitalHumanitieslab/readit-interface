e6c39d9b3863958c194c139a492e6886b89fecfd
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-11-26 16:02:02 +0100

    Align the triples in items.conftest with conftest.auth_client

--- backend/items/conftest.py
+++ backend/items/conftest.py
@@ -19,30 +19,30 @@ TRIPLES = (
     ( ITEM['1'], OA.prefix,             Literal('this is the start of ')   ),
     ( ITEM['1'], OA.exact,              Literal('the exact selection')     ),
     ( ITEM['1'], OA.suffix,             Literal(' and this is the end')    ),
-    ( ITEM['1'], DCTERMS.creator,       STAFF.AHebing                      ),
+    ( ITEM['1'], DCTERMS.creator,       STAFF.tester                       ),
     ( ITEM['1'], DCTERMS.created,       CREATION_DATE                      ),
 
     ( ITEM['2'], RDF.type,              OA.XPathSelector                   ),
     ( ITEM['2'], RDF.value,             Literal('substring(.//*[0]/text(), 22)') ),
-    ( ITEM['2'], DCTERMS.creator,       STAFF.AHebing                      ),
+    ( ITEM['2'], DCTERMS.creator,       STAFF.tester                       ),
     ( ITEM['2'], DCTERMS.created,       CREATION_DATE                      ),
 
     ( ITEM['3'], RDF.type,              OA.XPathSelector                   ),
     ( ITEM['3'], RDF.value,             Literal('substring(.//*[0]/text(), 41)') ),
-    ( ITEM['3'], DCTERMS.creator,       STAFF.AHebing                      ),
+    ( ITEM['3'], DCTERMS.creator,       STAFF.tester                       ),
     ( ITEM['3'], DCTERMS.created,       CREATION_DATE                      ),
 
     ( ITEM['4'], RDF.type,              VOCAB.RangeSelector                ),
     ( ITEM['4'], OA.hasStartSelector,   ITEM['2']                          ),
     ( ITEM['4'], OA.hasEndSelector,     ITEM['3']                          ),
-    ( ITEM['4'], DCTERMS.creator,       STAFF.AHebing                      ),
+    ( ITEM['4'], DCTERMS.creator,       STAFF.tester                       ),
     ( ITEM['4'], DCTERMS.created,       CREATION_DATE                      ),
 
     ( ITEM['5'], RDF.type,              OA.SpecificResource                ),
     ( ITEM['5'], OA.hasSource,          SOURCE['1']                        ),
     ( ITEM['5'], OA.hasSelector,        ITEM['4']                          ),
     ( ITEM['5'], OA.hasSelector,        ITEM['1']                          ),
-    ( ITEM['5'], DCTERMS.creator,       STAFF.AHebing                      ),
+    ( ITEM['5'], DCTERMS.creator,       STAFF.tester                       ),
     ( ITEM['5'], DCTERMS.created,       CREATION_DATE                      ),
 
     ( ITEM['6'], RDF.type,              ONTO.reader                        ),
@@ -57,7 +57,7 @@ TRIPLES = (
     ( ITEM['6'], CIDOC.died,            Literal(date(1849, 6, 4))          ),
     ( ITEM['6'], OWL.sameAs,            URIRef('https://en.wikipedia.org/wiki/Marguerite_Gardiner,_Countess_of_Blessington') ),
     ( ITEM['6'], DCTERMS.type,          Literal('example data')            ),
-    ( ITEM['6'], DCTERMS.creator,       STAFF.AHebing                      ),
+    ( ITEM['6'], DCTERMS.creator,       STAFF.tester                       ),
     ( ITEM['6'], DCTERMS.created,       CREATION_DATE                      ),
 
     ( ITEM['7'], RDF.type,              OA.Annotation                      ),
@@ -66,7 +66,7 @@ TRIPLES = (
     ( ITEM['7'], OA.hasTarget,          ITEM['5']                          ),
     ( ITEM['7'], OA.motivatedBy,        OA.tagging                         ),
     ( ITEM['7'], OA.motivatedBy,        OA.identifying                     ),
-    ( ITEM['7'], DCTERMS.creator,       STAFF.AHebing                      ),
+    ( ITEM['7'], DCTERMS.creator,       STAFF.tester                       ),
     ( ITEM['7'], DCTERMS.created,       CREATION_DATE                      ),
 )
 
