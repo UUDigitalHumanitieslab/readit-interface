981de0c3206cb00fdaae06d7f4d2131f4c03773d
Alex Hebing <a.h.j.hebing@uu.nl>
2019-11-29 13:30:30 +0100

    Fix single number utility (#173, #185)

--- frontend/src/utilities/binary-searchable-strategy/binary-search-utilities.ts
+++ frontend/src/utilities/binary-searchable-strategy/binary-search-utilities.ts
@@ -1,10 +1,5 @@
-
-export function singleNumber(nodeIndex: number, characterIndex: number): number {
-    return nodeIndex + 2 - (1 / (characterIndex + 1));
+export function singleNumber(mostSignificant: number, moreSignificant?: number, ...lessSignificant: number[]): number {
+    if (moreSignificant == null) return mostSignificant + 1;
+    const tail = singleNumber(moreSignificant, ...lessSignificant);
+    return -(1 / tail) + 2 + mostSignificant;
 }
-
-// function singleNumber(mostSignificant: number, moreSignificant: number, ...lessSignificant: number[]): number {
-//     if (moreSignificant == null) return mostSignificant + 1;
-//     const tail = singleNumber(moreSignificant, ...lessSignificant);
-//     return -(1 / tail) + 2 + mostSignificant;
-// }
