781bec5722add4002748e7b9c5ab11023bd12a3d
Alex Hebing <a.h.j.hebing@uu.nl>
2019-12-05 08:58:31 +0100

    Update the BinarySearchStrategy to full-blown automagicality (#185)

--- frontend/src/utilities/binary-searchable-strategy/binary-search-strategy-test.ts
+++ frontend/src/utilities/binary-searchable-strategy/binary-search-strategy-test.ts
@@ -1,20 +1,45 @@
-import { BinarySearchStrategy, BinarySearchableView } from './binary-search-strategy';
+import { BinarySearchStrategy } from './binary-search-strategy';
 import View from '../../core/view';
 
-describe('BinarySearchStrategy', function () {
+type TestView = {
+    view: View;
+    indexValue: number;
+};
 
+describe('BinarySearchStrategy', function () {
+    let testViews: TestView[];
     let strategy: BinarySearchStrategy;
-    let view1 = new View();
-    let view2 = new View();
-    let view3 = new View();
-    let view4 = new View();
+    let view1, view2, view3, view4;
+
+    /**
+     * Find the indexValue for a View that is in testViews.
+     */
+    function getIndexValue(view: View): number {
+        return testViews.find(v => v.view === view).indexValue;
+    }
+
+    /**
+     * Get an empty instance of View that is registered with in testViews.
+     */
+    function getView(indexValue: number): View {
+        let view = new View();
+        testViews.push({ indexValue: indexValue, view: view});
+        return view;
+    }
 
     beforeEach(function () {
-        strategy = new BinarySearchStrategy();
-        strategy.add({ indexValue: 1, view: view1 });
-        strategy.add({ indexValue: 3, view: view2 });
-        strategy.add({ indexValue: 6, view: view3 });
-        strategy.add({ indexValue: 9, view: view4 });
+        // always start with fresh four views
+        testViews = [];
+        view1 = getView(1);
+        view2 = getView(3);
+        view3 = getView(6);
+        view4 = getView(9);
+
+        strategy = new BinarySearchStrategy(getIndexValue);
+        strategy.add(view1);
+        strategy.add(view2);
+        strategy.add(view3);
+        strategy.add(view4);
     });
 
     afterEach(function () {
@@ -22,93 +47,143 @@ describe('BinarySearchStrategy', function () {
 
     describe('add', function () {
         it('adds a searchable at the correct index', function () {
-            let view5 = new View();
-            strategy.add({ indexValue: 5, view: view5 });
-            expect(strategy.searchables.length).toEqual(5);
-            expect(strategy.searchables[2].view).toEqual(view5);
+            let view5 = getView(5);
+            strategy.add(view5);
+            expect(strategy.views.length).toEqual(5);
+            expect(strategy.views[2]).toEqual(view5);
         });
 
-        it('adds a searchable with same indexValue after all existing searchables with same indexValue', function () {
-            let view5 = new View();
-            strategy.add({ indexValue: 6, view: view5 });
-            expect(strategy.searchables.length).toEqual(5);
-            expect(strategy.searchables[3].view).toEqual(view5);
+        it('adds a searchable with same indexValue after all existing views with same indexValue', function () {
+            let view5 = getView(6)
+            strategy.add(view5);
+            expect(strategy.views.length).toEqual(5);
+            expect(strategy.views[3]).toEqual(view5);
 
-            let view6 = new View();
-            strategy.add({ indexValue: 9, view: view6 });
-            expect(strategy.searchables.length).toEqual(6);
-            expect(strategy.searchables[5].view).toEqual(view6);
+            let view6 = getView(9);
+            strategy.add(view6);
+            expect(strategy.views.length).toEqual(6);
+            expect(strategy.views[5]).toEqual(view6);
 
-            let view7 = new View();
-            strategy.add({ indexValue: 9, view: view7 });
-            expect(strategy.searchables.length).toEqual(7);
-            expect(strategy.searchables[6].view).toEqual(view7);
+            let view7 = getView(9);
+            strategy.add(view7);
+            expect(strategy.views.length).toEqual(7);
+            expect(strategy.views[6]).toEqual(view7);
         });
     });
 
     describe('remove', function () {
         it('deletes a searchable view', function () {
-            strategy.remove({ indexValue: 6, view: view3 });
-            expect(strategy.searchables.length).toEqual(3);
+            strategy.remove(view3);
+            expect(strategy.views.length).toEqual(3);
         });
 
-        it('deletes the correct searchable view if multiple exist with same indexValue', function () {
-            let view5 = new View();
-            let searchable = { indexValue: 6, view: view5 };
-            strategy.add(searchable);
-            expect(strategy.searchables.length).toEqual(5);
-            strategy.remove(searchable);
-            expect(strategy.searchables.length).toEqual(4);
+        it('deletes the correct view if multiple exist with same indexValue', function () {
+            let view5 = getView(6);
+            strategy.add(view5);
+            expect(strategy.views.length).toEqual(5);
+            strategy.remove(view5);
+            expect(strategy.views.length).toEqual(4);
 
-            expect(strategy.searchables).toEqual([
-                { indexValue: 1, view: view1 },
-                { indexValue: 3, view: view2 },
-                { indexValue: 6, view: view3 },
-                { indexValue: 9, view: view4 }
+            expect(strategy.views).toEqual([
+                view1, view2, view3, view4
             ]);
 
-            strategy.add(searchable);
-            expect(strategy.searchables.length).toEqual(5);
-            strategy.remove({ indexValue: 6, view: view3 });
-            expect(strategy.searchables.length).toEqual(4);
+            strategy.add(view5);
+            expect(strategy.views.length).toEqual(5);
+            strategy.remove(view3);
+            expect(strategy.views.length).toEqual(4);
 
-            expect(strategy.searchables).toEqual([
-                { indexValue: 1, view: view1 },
-                { indexValue: 3, view: view2 },
-                { indexValue: 6, view: view5 },
-                { indexValue: 9, view: view4 }
+            expect(strategy.views).toEqual([
+                view1, view2, view5, view4
             ]);
         });
 
         it('doesn\'t break if we remove an unknown view', function () {
             // try to delete unknown indexValue
-            strategy.remove({ indexValue: 10, view: new View() });
-            expect(strategy.searchables.length).toEqual(4);
+            let unknownIndexView = getView(10);
+            strategy.remove(unknownIndexView);
+            expect(strategy.views.length).toEqual(4);
 
             // try to delete known indexValue, but with unknown view
-            strategy.remove({ indexValue: 6, view: new View() });
-            expect(strategy.searchables.length).toEqual(4);
+            let unknownView = getView(6);
+            strategy.remove(unknownView);
+            expect(strategy.views.length).toEqual(4);
         });
     });
 
-    describe('getClosestTo', function () {
-        it('finds a view at index closest to', function () {
-            let actual = strategy.getClosestTo(2);
+    describe('lastLessThan', function () {
+        it('finds a view at index smaller than', function () {
+            let actual = strategy.lastLessThan(2);
             expect(actual).toEqual(view1);
-            actual = strategy.getClosestTo(7);
+            actual = strategy.lastLessThan(7);
             expect(actual).toEqual(view3);
         });
 
+        it('does not return view with exactly matching indexValue if it exists', function () {
+            let actual = strategy.lastLessThan(6);
+            expect(actual).not.toEqual(view3);
+            expect(actual).toEqual(view2);
+        });
+    });
+
+    describe('equalToOrLastLessThan', function () {
+        it('finds a view at index if it exists', function () {
+            let actual = strategy.equalToOrLastLessThan(1);
+            expect(actual).toEqual(view1);
+            actual = strategy.equalToOrLastLessThan(3);
+            expect(actual).toEqual(view2);
+        });
+
+        it('finds a view at index smaller than if indexValue does not exist', function () {
+            let actual = strategy.equalToOrLastLessThan(2);
+            expect(actual).toEqual(view1);
+        });
+    });
+
+    describe('firstNotLessThan', function () {
         it('returns view with exactly matching indexValue if it exists', function () {
-            let actual = strategy.getClosestTo(6);
+            let actual = strategy.firstNotLessThan(1);
+            expect(actual).toEqual(view1);
+            actual = strategy.firstNotLessThan(3);
+            expect(actual).toEqual(view2);
+        });
+
+        it('returns first view with exactly matching indexValue if multiple exists', function () {
+            let newerView = getView(6);
+            strategy.add(newerView);
+            let actual = strategy.firstNotLessThan(6);
+            expect(actual).toEqual(view3);
+        });
+    });
+
+    describe('lastNotGreaterThan', function () {
+        it('returns view with exactly matching indexValue if it exists', function () {
+            let actual = strategy.lastNotGreaterThan(1);
+            expect(actual).toEqual(view1);
+            actual = strategy.lastNotGreaterThan(3);
+            expect(actual).toEqual(view2);
+        });
+
+        it('returns last view with exactly matching indexValue if multiple exists', function () {
+            let newerView = getView(6);
+            strategy.add(newerView);
+            let actual = strategy.lastNotGreaterThan(6);
+            expect(actual).toEqual(newerView);
+        });
+    });
+
+    describe('firstGreaterThan', function () {
+        it('finds first view with indexValue greater than', function () {
+            let actual = strategy.firstGreaterThan(1);
+            expect(actual).toEqual(view2);
+            actual = strategy.firstGreaterThan(3);
             expect(actual).toEqual(view3);
         });
 
-        it('doesn\'t care if an indexValue exists multiple time', function () {
-            let view5 = new View();
-            strategy.add({ indexValue: 6, view: view5 });
-            let actual = strategy.getClosestTo(6);
-            expect(actual).toEqual(view3);
+        it('does not return view with exactly matching indexValue if it exists', function () {
+            let actual = strategy.firstGreaterThan(6);
+            expect(actual).not.toEqual(view3);
+            expect(actual).toEqual(view4);
         });
     });
 });
--- frontend/src/utilities/binary-searchable-strategy/binary-search-strategy.ts
+++ frontend/src/utilities/binary-searchable-strategy/binary-search-strategy.ts
@@ -1,58 +1,105 @@
-import { sortedLastIndexBy, sortedIndexBy } from 'lodash';
+import { sortedLastIndexBy, sortedIndexBy, bind } from 'lodash';
 import View from "../../core/view";
 
-export type BinarySearchableView = {
-    indexValue: number,
-    view: View
+
+export class FakeView extends View {
+    indexValue: number;
+    constructor(indexValue: number) {
+        super();
+        this.indexValue = indexValue;
+    }
 }
 
 export class BinarySearchStrategy {
-    searchables: BinarySearchableView[];
+    views: View[];
+    getIndexValue: (view: View) => number;
 
-    constructor() {
-        this.searchables = [];
+    constructor(getIndexValue: (view: View) => number) {
+        this.views = [];
+        this.getIndexValue = getIndexValue;
     }
 
     /**
-     * Add a searchable to the strategy's list of searchables.
-     * The new searchable will be added at the index corresponding to its indexValue.
-     * Note that if an indexValue already exists, the new searchable will be added
+     * Add a view to the strategy's list of views.
+     * The new view will be added at the index corresponding to its indexValue.
+     * Note that if an indexValue already exists, the new view will be added
      * AFTER the existing one(s).
      */
-    add(searchable: BinarySearchableView): this {
-        let index = sortedLastIndexBy(this.searchables, searchable, 'indexValue');
-        this.searchables.splice(index, 0, searchable);
+    add(view: View): this {
+        let index = sortedLastIndexBy(this.views, view, this.getIndexValue);
+        this.views.splice(index, 0, view);
         return this;
     }
 
     /**
-     * Remove a searchable from the strategy's list of searchables.
+     * Remove a view from the strategy's list of views.
      */
-    remove(searchable: BinarySearchableView): this {
-        let index = sortedIndexBy(this.searchables, searchable, 'indexValue');
-        let lowerBound = sortedIndexBy(this.searchables, searchable, 'indexValue');
-        let upperBound = sortedLastIndexBy(this.searchables, searchable, 'indexValue');
+    remove(view: View): this {
+        let index;
+        let lowerBound = sortedIndexBy(this.views, view, this.getIndexValue);
+        let upperBound = sortedLastIndexBy(this.views, view, this.getIndexValue);
 
-        for (let index = lowerBound; index < upperBound; ++index) {
-            if (this.searchables[index].view === searchable.view) break;
+        for (index = lowerBound; index < upperBound; ++index) {
+            if (this.views[index] === view) break;
         }
 
-        if (index < upperBound) this.searchables.splice(index, 1);
+        if (index < upperBound) this.views.splice(index, 1);
         return this;
     }
 
     /**
-     * Get the View closest to an indexValue. 'Closest to' here means the value
-     * before the supplied indexValue, i.e. the nearest smaller one.
+     * Helper function to fo find an index without having an instance of View. i.e. only an indexValue.
      */
-    getClosestTo(indexValue: number): View {
-        let fakeSearchable = {
-            indexValue: indexValue,
-            view: null
+    getIndexValueWithFakeView(view: View): number {
+        if (view instanceof FakeView) {
+            return view.indexValue;
         }
-        let index = sortedIndexBy(this.searchables, fakeSearchable, 'indexValue');
-        let searchable = this.searchables[index];
-        if (!searchable || searchable.indexValue !== indexValue) searchable = this.searchables[index - 1];
-        return searchable.view;
+        else return this.getIndexValue(view);
+    }
+
+    /**
+     * Returns the last View with indexValue BEFORE the supplied indexValue, i.e. the nearest smaller one.
+     * For example, given 5, from [ 1, 2, 3, 4, 5, 5, 7, 8] it returns 4.
+     */
+    lastLessThan(indexValue: number): View {
+        let index = sortedIndexBy(this.views, new FakeView(indexValue), this.getIndexValueWithFakeView.bind(this));
+        return this.views[index - 1];
+    }
+
+    /**
+     * Returns the first View with the exact indexValue if it exists,
+     * or the View with indexValue BEFORE the supplied indexValue, i.e. the nearest smaller one.
+     */
+    equalToOrLastLessThan(indexValue: number): View {
+        let view = this.firstNotLessThan(indexValue);
+        if (indexValue == this.getIndexValue(view)) return view;
+        return this.lastLessThan(indexValue);
+    }
+
+    /**
+     * Returns the first View with indexValue not smaller than the supplied indexValue.
+     * For example, given 5, from [ 1, 2, 3, 4, 5, 5, 7, 8] it returns the left 5.
+     */
+    firstNotLessThan(indexValue: number): View {
+        let index = sortedIndexBy(this.views, new FakeView(indexValue), this.getIndexValueWithFakeView.bind(this));
+        return this.views[index];
+    }
+
+    /**
+     * Returns the last View with indexValue not greater than the supplied indexValue.
+     * For example, given 5, from [ 1, 2, 3, 4, 5, 5, 7, 8] it returns the right 5.
+     */
+    lastNotGreaterThan(indexValue: number): View {
+        let index = sortedLastIndexBy(this.views, new FakeView(indexValue), this.getIndexValueWithFakeView.bind(this));
+        return this.views[index - 1];
+    }
+
+    /**
+     * Returns the first View with indexValue greater than the supplied indexValue.
+     * For example, given 5, from [ 1, 2, 3, 4, 5, 5, 7, 8] it returns 6.
+     */
+    firstGreaterThan(indexValue: number): View {
+        let index = sortedLastIndexBy(this.views, new FakeView(indexValue), this.getIndexValueWithFakeView.bind(this));
+        return this.views[index];
     }
 }
