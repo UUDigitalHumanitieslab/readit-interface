4a864cb86d70e37808ee315e6345c400f4617c61
Alex Hebing <a.h.j.hebing@uu.nl>
2020-01-20 15:03:25 +0100

    Add validation on frontend (#214)

--- frontend/src/user/registration/registration-view.ts
+++ frontend/src/user/registration/registration-view.ts
@@ -7,11 +7,28 @@ export default class RegistrationFormView extends View {
 
     render(): this {
         this.$el.html(this.template({}));
+        this.$el.validate({
+            errorClass: "help is-danger",
+            rules: {
+                password: "required",
+                password_again: {
+                    equalTo: "#password"
+                }
+            },
+        })
+        return this;
+    }
+
+    onRegisterClicked(event: JQueryEventObject): this {
+        console.log(this.$el.valid());
         return this;
     }
 }
 extend(RegistrationFormView.prototype, {
     tagName: 'form',
     className: 'registration-form section page',
+    events: {
+        'submit': 'onRegisterClicked',
+    },
     template: registrationTemplate
 });
