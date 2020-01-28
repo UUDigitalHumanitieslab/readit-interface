52405aa32bf436aee8f48d779108f85c2a1a3b53
Julian Gonggrijp <j.gonggrijp@uu.nl>
2020-01-28 15:53:39 +0100

    Only login on refresh if needed (close #69)
    @alexhebing no more fiddling with manually disabling the login!

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,11 +6,13 @@ import { baseUrl } from 'config.json';
 import { i18nPromise } from './global/i18n';
 import './global/internalLinks';
 import './global/hbsHelpers';
+import user from './global/user';
 import { prefetch } from './global/ld-store';
 import './aspects/readit';
 import './aspects/authentication';
 
 when(ready, i18nPromise).done(function() {
+    user.fetch();
     let success = history.start({
         root: baseUrl,
         pushState: true,
