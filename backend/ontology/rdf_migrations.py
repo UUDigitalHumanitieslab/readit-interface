28c027a9ff4d9e5d7caa0f22afd1ba9a5051dce2
Alex Hebing <a.h.j.hebing@uu.nl>
2020-01-29 08:38:14 +0100

    Rename all triples with state of mind as object to reading response (#216)

--- backend/ontology/rdf_migrations.py
+++ backend/ontology/rdf_migrations.py
@@ -9,6 +9,16 @@ from .fixture import canonical_graph
 CIDOC = Namespace('http://www.cidoc-crm.org/cidoc-crm/')
 
 
+def replace_object(graph, before, after):
+    """
+    Replace all triples with `before` as object by one with `after`.
+    """
+    obsolete = list(graph.quads((None, None, before)))
+    prune_triples(graph, obsolete)
+    replacements = ((s, p, after, c) for (s, p, o, c) in obsolete)
+    append_triples(graph, replacements)
+
+
 def replace_predicate(graph, before, before_rev, after):
     """
     Replace all triples with `before` as predicate by one with `after`.
@@ -51,3 +61,9 @@ class Migration(RDFMigration):
         before_rev = CIDOC.was_influenced_by
         after = READIT.influenced
         replace_predicate(conjunctive, before, before_rev, after)
+
+    @on_remove(READIT.state_of_mind)
+    def replace_READIT_state_of_mind(self, actual, conjunctive):
+        before = READIT.state_of_mind
+        after = READIT.reading_response
+        replace_object(conjunctive, before, after)
