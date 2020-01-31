adb2dd22bd264c2bd755f12140876397b2e6f0f2
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-01-31 14:03:41 +0100

    Correct the relation editor: use RangePicker and fix selectors (#46)

--- frontend/src/panel-related-items/relation-editor-view.ts
+++ frontend/src/panel-related-items/relation-editor-view.ts
@@ -2,46 +2,65 @@ import { extend } from 'lodash';
 
 import Model from '../core/model';
 import { CompositeView } from '../core/view';
+import ldChannel from '../jsonld/radio';
 import { rdf } from '../jsonld/ns';
 import Node from '../jsonld/node';
 import PickerView from '../forms/base-picker-view';
+import RangePicker from '../forms/range-picker-view';
 import ItemGraph from '../utilities/item-graph';
 import relationTemplate from './relation-editor-template';
 
 export default class RelationEditor extends CompositeView {
     predicatePicker: PickerView;
-    objectPicker: PickerView;
-    objectOptions: ItemGraph;
+    objectPicker: RangePicker;
 
     initialize() {
         this.predicatePicker = new PickerView({collection: this.collection});
-        this.objectOptions = new ItemGraph();
-        this.objectPicker = new PickerView({collection: this.objectOptions});
         this.predicatePicker.on('change', this.updatePredicate, this);
-        this.objectPicker.on('change', this.updateObject, this);
         if (!this.model) this.model = new Model();
-        this.listenTo(this.model, 'change:predicate', this.predicateFromModel);
-        this.listenTo(this.model, 'change:object', this.objectFromModel);
         this.predicateFromModel(this.model).objectFromModel(this.model);
+        this.render();
+    }
+
+    renderContainer(): this {
+        this.$el.html(this.template(this));
+        return this;
     }
 
     updatePredicate(picker: PickerView, id: string): void {
-        this.model.set('predicate', this.collection.get(id));
+        const predicate = this.collection.get(id);
+        this.model.set('predicate', predicate);
+        this.model.unset('object');
+        this.resetObjectPicker(predicate);
     }
 
-    updateObject(picker: PickerView, id: string): void {
-        this.model.set('object', this.collection.get(id));
+    updateObject(picker: RangePicker, id: string): void {
+        this.model.set('object', picker.collection.get(id));
+    }
+
+    resetObjectPicker(predicate: Node): this {
+        if (this.objectPicker) {
+            if (this.objectPicker.model === predicate) return this;
+            this.objectPicker.remove();
+        }
+        this.objectPicker = this.createPicker(predicate);
+        ldChannel.trigger('cache:items', this.objectPicker.admittedTypes);
+        this.objectPicker.on('change', this.updateObject, this);
+        return this;
+    }
+
+    createPicker(predicate: Node): RangePicker {
+        return ldChannel.request('visit', store => new RangePicker({
+            model: predicate,
+            collection: store,
+        }));
     }
 
     predicateFromModel(model: Model, selectedPredicate?: Node): this {
         selectedPredicate || (selectedPredicate = model.get('predicate'));
         if (!selectedPredicate) return this;
-        this.objectOptions.query({
-            predicate: rdf.type,
-            object: selectedPredicate.id,
-        });
         this.predicatePicker.val(selectedPredicate.id);
-        return this;
+        return this.resetObjectPicker(selectedPredicate);
     }
 
     objectFromModel(model: Model, selectedObject?: Node): this {
@@ -63,9 +82,9 @@ extend(RelationEditor.prototype, {
     },
     subviews: [{
         view: 'predicatePicker',
-        selector: '.control:first',
+        selector: '.control:first-child',
     }, {
         view: 'objectPicker',
-        selector: '.control:last',
+        selector: '.control:nth-child(2)',
     }],
 });
