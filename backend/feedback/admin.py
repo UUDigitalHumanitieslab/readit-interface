f1ad0e0e369644eae3b42853c1049f11496fa6eb
Alex Hebing <a.h.j.hebing@uu.nl>
2020-02-28 10:25:02 +0100

    Update feedback admin to display provided_by (#164)

--- backend/feedback/admin.py
+++ backend/feedback/admin.py
@@ -4,8 +4,8 @@ from .models import Feedback
 
 class FeedbackAdmin(admin.ModelAdmin):
     model = Feedback
-    readonly_fields = ('date_created', 'subject', 'feedback')
-    fields = ['date_created', 'subject', 'feedback', 'label',]
+    readonly_fields = ('date_created', 'subject', 'feedback', 'provided_by')
+    fields = ['date_created', 'subject', 'feedback', 'provided_by', 'label',]
     list_display = ('date_created', 'subject', 'label')
     list_filter = ('date_created', 'label')
 
