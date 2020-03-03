18a3a64aaf30f255f27bf8c2f811d6b499e66ffd
Alex Hebing <a.h.j.hebing@uu.nl>
2020-03-03 14:51:20 +0100

    Update feedback model after feedback (#258)

--- backend/feedback/models.py
+++ backend/feedback/models.py
@@ -1,20 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
 
 # Create your models here.
 class Feedback(models.Model):
     subject = models.CharField(max_length=150, blank=True)
     label = models.CharField(max_length=150, blank=True, default='Not labelled yet')
     feedback = models.TextField()
-    date_created = models.DateTimeField()
+    date_created = models.DateTimeField(auto_now_add=True)
     provided_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name_plural = "feedback"
-
-    def save(self, *args, **kwargs):
-        ''' If new, set the created date '''
-        if self.pk is None:
-            self.date_created = datetime.now()
-        return super(Feedback, self).save(*args, **kwargs)
