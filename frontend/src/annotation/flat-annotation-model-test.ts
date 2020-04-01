a94b109a12e1240fac059cea1cf226e6ba0ed809
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-04-01 13:23:58 +0200

    Add FlatAnnotationModel
    Related to #237, #124.

---
+++ frontend/src/annotation/flat-annotation-model-test.ts
@@ -0,0 +1,168 @@
+import { mapValues, invert, pick, assign, omit, each, delay } from 'lodash';
+import { Events } from 'backbone';
+
+import { startStore, endStore } from '../test-util';
+import { contentClass, readerClass } from '../mock-data/mock-ontology';
+import mockItems from '../mock-data/mock-items';
+import { skos, dcterms, oa, readit, item } from '../jsonld/ns';
+import { asNative } from '../jsonld/conversion';
+import Node from '../jsonld/node';
+import FlatAnnotation from './flat-annotation-model';
+
+interface NodeMap {
+    [key: string]: Node;
+}
+
+const itemAttributes = mockItems.slice(0, 7);
+const itemKeys = 'annotation item target range start end text'.split(' ');
+const itemIndex = invert(itemKeys);
+
+const expectedFlatAttributes = {
+    annotation: jasmine.any(Node),
+    id: item('100'),
+    class: jasmine.any(Node),
+    cssClass: 'is-readit-content',
+    item: jasmine.any(Node),
+    label: 'The Idler in France',
+    source: jasmine.any(Node),
+    startPosition: 15,
+    endPosition: 34,
+    text: 'The Idler in France',
+    prefix: 'English descriptions of reading experiences <br><br> id_19 Titre : ',
+    suffix: ' / by the countess of Blessington Auteur : Blessington,',
+    creator: jasmine.any(Node),
+    created: jasmine.any(Date),
+};
+
+function createPlaceholder(attributes): Node {
+    return new Node(pick(attributes, '@id'));
+}
+
+function getPlaceholders(): NodeMap {
+    return mapValues(itemIndex, idx => createPlaceholder(itemAttributes[idx]));
+}
+
+function getFullItems(): NodeMap {
+    return mapValues(itemIndex, idx => new Node(itemAttributes[idx]));
+}
+
+// Helper to make the `name` event on `emitter` `await`-able.
+// Caveat: only resolves if the event triggers *after* you call this function.
+function event(emitter: Events, name: string): Promise<void> {
+    return new Promise(resolve => emitter.once(name, resolve));
+}
+
+// Helper to make a `FlatAnnotation`'s `'complete'` event `await`-able.
+// As a special case, this one also works if the event already triggered.
+function completion(anno: FlatAnnotation): Promise<void> {
+    return (anno.completed) ? Promise.resolve() : event(anno, 'complete');
+}
+
+// Helper to make a timeout `await`-able.
+function timeout(milliseconds): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+describe('FlatAnnotationModel', function() {
+    beforeEach(startStore);
+    afterEach(endStore);
+
+    it('does not do much until data arrive', async function() {
+        const items = getPlaceholders();
+        const spy = jasmine.createSpy();
+        const flatAnno = new FlatAnnotation(items.annotation);
+        flatAnno.once('complete', spy);
+        await timeout(50);
+        expect(spy).not.toHaveBeenCalled();
+        expect(flatAnno.completed).toBe(false);
+        expect(flatAnno.attributes).toEqual({});
+    });
+
+    it('flattens data that are there from the start', async function() {
+        const items = getFullItems();
+        const ontologyClass = new Node(contentClass);
+        const spy = jasmine.createSpy();
+        const flatAnno = new FlatAnnotation(items.annotation);
+        flatAnno.on('complete', spy);
+        await completion(flatAnno);
+        expect(flatAnno.completed).toBe(true);
+        expect(flatAnno.attributes).toEqual(expectedFlatAttributes);
+        await timeout(50);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('completes without an item when no item is expected', async function() {
+        const items = getFullItems();
+        const ontologyClass = new Node(contentClass);
+        items.annotation.unset(oa.hasBody, items.item);
+        items.item.clear();
+        const flatAnno = new FlatAnnotation(items.annotation);
+        await completion(flatAnno);
+        expect(flatAnno.completed).toBe(true);
+        expect(flatAnno.attributes).toEqual(
+            omit(expectedFlatAttributes, ['item', 'label'])
+        );
+    });
+
+    it('flattens data that arrive with a delay', async function() {
+        const items = getPlaceholders();
+        const ontologyClass = createPlaceholder(contentClass);
+        const flatAnno = new FlatAnnotation(items.annotation);
+        each(itemAttributes, (attributes, index) => delay(
+            () => items[itemKeys[index]].set(attributes),
+            (index + 1) * 10
+        ));
+        await timeout(80);
+        expect(flatAnno.completed).toBe(false);
+        ontologyClass.set(contentClass);
+        await completion(flatAnno);
+        expect(flatAnno.completed).toBe(true);
+        expect(flatAnno.attributes).toEqual(expectedFlatAttributes);
+    });
+
+    it('updates the class and item after the fact', async function() {
+        const items = getFullItems();
+        const ontologyClass = new Node(contentClass);
+        const flatAnno = new FlatAnnotation(items.annotation);
+        await completion(flatAnno);
+
+        const replacementItem = new Node({
+            '@id': item('1000'),
+            '@type': ontologyClass.id,
+            [skos.prefLabel]: {'@value': 'The slacker in Bohemia'},
+        });
+        items.annotation.unset(oa.hasBody, items.item, {silent: true});
+        items.annotation.set(oa.hasBody, replacementItem);
+        await event(flatAnno, 'change');
+        expect(flatAnno.get('label')).toBe('The slacker in Bohemia');
+        expect(flatAnno.get('item')).toBe(replacementItem);
+        expect(flatAnno.get('cssClass')).toBe(expectedFlatAttributes.cssClass);
+        expect(flatAnno.get('class')).toBe(ontologyClass);
+
+        const replacementClass = new Node(readerClass);
+        items.annotation.unset(oa.hasBody, ontologyClass, {silent: true});
+        items.annotation.set(oa.hasBody, replacementClass);
+        await event(flatAnno, 'change');
+        expect(flatAnno.get('label')).toBe('The slacker in Bohemia');
+        expect(flatAnno.get('item')).toBe(replacementItem);
+        expect(flatAnno.get('cssClass')).toBe('is-readit-reader');
+        expect(flatAnno.get('class')).toBe(replacementClass);
+    });
+
+    it('cannot be tricked into completing multiple times', async function() {
+        const items = getFullItems();
+        const ontologyClass = new Node(contentClass);
+        const flatAnno = new FlatAnnotation(items.annotation);
+        await completion(flatAnno);
+        const spy = jasmine.createSpy();
+        flatAnno.on('complete', spy);
+        // unset and reset one flag
+        flatAnno._completionFlags ^= 32;
+        flatAnno._setCompletionFlag(32);
+        // unset and reset all flags
+        flatAnno._completionFlags = 0;
+        flatAnno._setCompletionFlag(127);
+        await timeout(50);
+        expect(spy).not.toHaveBeenCalled();
+    });
+});
---
+++ frontend/src/annotation/flat-annotation-model.ts
@@ -0,0 +1,239 @@
+import { noop, defer, each, includes } from 'lodash';
+
+import Model from '../core/model';
+import { rdf, dcterms, oa, vocab, readit, item } from '../jsonld/ns';
+import Node from '../jsonld/node';
+import { getLabel, getCssClassName } from '../utilities/utilities';
+import { getCharacterIndex } from '../utilities/annotation/annotation-utilities';
+
+/**
+ * Flag bitmasks for tracking completion of a flat annotation. For a quick
+ * introduction to flags and bitmasks, see this Stack Overflow answer:
+ * https://stackoverflow.com/questions/15317723/what-are-flags-and-bitfields#15317846
+ * In addition, you may want to review JavaScript's bitwise operators:
+ * https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators
+ */
+const F_ID       = 1;
+const F_CSSCLASS = 1 << 1;
+const F_LABEL    = 1 << 2;
+const F_SOURCE   = 1 << 3;
+const F_STARTPOS = 1 << 4;
+const F_ENDPOS   = 1 << 5;
+const F_TEXT     = 1 << 6;
+const F_COMPLETE = F_ID | F_CSSCLASS | F_LABEL | F_SOURCE | F_STARTPOS | F_ENDPOS | F_TEXT;
+
+/**
+ * Name for the type of function that takes a `Node` as its first parameter.
+ */
+interface Processor {
+    (node: Node): void;
+}
+
+/**
+ * Adapter that transforms a `Node` representing an `oa.Annotation`s into a
+ * flattened representation that is easier to process. In this representation,
+ * each direct and indirect RDF property of interest is mapped to a direct
+ * model attribute.
+ *
+ * Please keep in mind that this transform is one way only. In order to edit an
+ * annotation, you have to walk the original RDF datastructure. However, the
+ * flat representation will update live with any changes to the underlying data.
+ *
+ * The RDF properties are mapped to model attributes as follows:
+
+    annotation    = (original annotation)
+    id            = @id
+    class         = oa.hasBody (if ontology class)
+    cssClass      = oa.hasBody (if ontology class) -> getCssClassName()
+    item          = oa.hasBody (if item)
+    label         = oa.hasBody (if item) -> getLabel()
+    source        = oa.hasTarget -> oa.hasSource
+    startPosition = oa.hasTarget -> oa.hasSelector (if vocab.RangeSelector) -> oa.hasStartSelector -> rdf.value -> parsed numeric value
+    endPosition   = oa.hasTarget -> oa.hasSelector (if vocab.RangeSelector) -> oa.hasEndSelector -> rdf.value -> parsed numeric value
+    text          = oa.hasTarget -> oa.hasSelector (if oa.TextQuoteSelector) -> oa.exact
+    prefix        = oa.hasTarget -> oa.hasSelector (if oa.TextQuoteSelector) -> oa.prefix
+    suffix        = oa.hasTarget -> oa.hasSelector (if oa.TextQuoteSelector) -> oa.suffix
+    creator       = dcterms.creator
+    created       = dcterms.created
+
+ * For further processing convenience, the model triggers a `'complete'` event
+ * when all of the following attributes have been collected: `id`, `cssClass`,
+ * `label`, `source`, `startPosition`, `endPosition`, `text`. It also exposes a
+ * read-only `completed` property which evaluates to `false` before the event
+ * and `true` after the event.
+ */
+export default class FlatAnnotationModel extends Model {
+    // Private bitfield for tracking completion.
+    _completionFlags: number;
+
+    // Public read-only property to check for completion.
+    get completed(): boolean {
+        return this._completionFlags === F_COMPLETE;
+    }
+
+    // Private method for updating the completion bitfield.
+    _setCompletionFlag(flag: number): void {
+        this._completionFlags |= flag;
+        if (this.completed) {
+            this.trigger('complete', this, this.get('annotation'));
+            // After this, we never need to check the flags or trigger the
+            // `'complete'` event again.
+            this._setCompletionFlag = noop;
+        }
+    }
+
+    /**
+     * Unlike most `Model` subclasses, this one accepts an underlying `Node`
+     * instead of an initial hash of attributes. Precondition: `annotation`
+     * must be an instance of `oa.Annotation`.
+     */
+    constructor(annotation: Node, options?: any) {
+        super(null, options);
+        this._completionFlags = 0;
+        this.handleWhenReady(annotation, oa.hasBody, this.receiveAnnotation);
+    }
+
+    /**
+     * Pattern for handling a `Node` that may still be a placeholder at the
+     * time of processing. This is determined by testing whether `attribute` is
+     * present on `node`. `handler` is always bound to `this` and always
+     * invoked async.
+     */
+    handleWhenReady(node: Node, attribute: string, handler: Processor): void {
+        if (node.has(attribute)) {
+            defer(handler.bind(this), node);
+        } else {
+            this.listenToOnce(node, `change:${attribute}`, handler);
+        }
+    }
+
+    /**
+     * Invoked once when the annotation is more than just a placeholder.
+     */
+    receiveAnnotation(annotation: Node): void {
+        this.set({
+            annotation,
+            id: annotation.id,
+            creator: annotation.get(dcterms.creator)[0],
+            created: annotation.get(dcterms.created)[0],
+        });
+        this.updateBodies(annotation);
+        this.listenTo(annotation, `change:${oa.hasBody}`, this.updateBodies);
+        const target = annotation.get(oa.hasTarget)[0] as Node;
+        this.handleWhenReady(target, oa.hasSelector, this.receiveTarget);
+        this._setCompletionFlag(F_ID);
+    }
+
+    /**
+     * Invoked every time oa.hasBody changes.
+     */
+    updateBodies(annotation: Node): void {
+        const bodies = annotation.get(oa.hasBody);
+        each(bodies, body => this.handleWhenReady(
+            body as Node,
+            '@type',
+            this.receiveBody
+        ));
+        // An annotation can be complete without an item.
+        if (bodies.length < 2) this._setCompletionFlag(F_LABEL);
+    }
+
+    /**
+     * Invoked once for each oa.hasBody when it is more than just a placeholder.
+     */
+    receiveBody(body: Node): void {
+        const id = body.id;
+        if (id.startsWith(readit())) return this.receiveClass(body);
+        if (id.startsWith(item())) return this.receiveItem(body);
+        // We can add another line like the above to add support for
+        // preannotations.
+    }
+
+    /**
+     * Invoked once when the class body is more than just a placeholder.
+     */
+    receiveClass(body: Node): void {
+        this.set({
+            class: body,
+            cssClass: getCssClassName(body),
+        });
+        this._setCompletionFlag(F_CSSCLASS);
+    }
+
+    /**
+     * Invoked once when the item body is more than just a placeholder.
+     */
+    receiveItem(body: Node): void {
+        this.set({
+            item: body,
+            label: getLabel(body),
+        });
+        this._setCompletionFlag(F_LABEL);
+    }
+
+    /**
+     * Invoked once when the target is more than just a placeholder.
+     */
+    receiveTarget(target: Node): void {
+        this.set('source', target.get(oa.hasSource)[0]);
+        const selectors = target.get(oa.hasSelector);
+        each(selectors, selector => this.handleWhenReady(
+            selector as Node,
+            '@type',
+            this.receiveSelector
+        ));
+        this._setCompletionFlag(F_SOURCE);
+    }
+
+    /**
+     * Invoked once for each oa.hasSelector when it is more than just a
+     * placeholder.
+     */
+    receiveSelector(selector: Node): void {
+        const type = selector.get('@type') as string[];
+        if (includes(type, vocab('RangeSelector'))) {
+            return this.receiveRange(selector);
+        }
+        if (includes(type, oa.TextQuoteSelector)) {
+            return this.receiveText(selector);
+        }
+    }
+
+    /**
+     * Invoked once when the RangeSelector is more than just a placeholder.
+     */
+    receiveRange(selector: Node): void {
+        const startSelector = selector.get(oa.hasStartSelector)[0] as Node;
+        const endSelector = selector.get(oa.hasEndSelector)[0] as Node;
+        this.handleWhenReady(startSelector, rdf.value, this.receiveStart);
+        this.handleWhenReady(endSelector, rdf.value, this.receiveEnd);
+    }
+
+    /**
+     * Invoked once when the start selector is more than just a placeholder.
+     */
+    receiveStart(selector: Node): void {
+        this.set('startPosition', getCharacterIndex(selector));
+        this._setCompletionFlag(F_STARTPOS);
+    }
+
+    /**
+     * Invoked once when the end selector is more than just a placeholder.
+     */
+    receiveEnd(selector: Node): void {
+        this.set('endPosition', getCharacterIndex(selector));
+        this._setCompletionFlag(F_ENDPOS);
+    }
+
+    /**
+     * Invoked once when the TextQuoteSelector is more than just a placeholder.
+     */
+    receiveText(selector: Node): void {
+        this.set({
+            text: selector.get(oa.exact)[0],
+            prefix: selector.get(oa.prefix)[0],
+            suffix: selector.get(oa.suffix)[0],
+        });
+        this._setCompletionFlag(F_TEXT);
+    }
+}
