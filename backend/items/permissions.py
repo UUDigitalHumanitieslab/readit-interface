c3fd2a07f1b47cae6100e163c4c510c279b2225c
Alex Hebing <a.h.j.hebing@uu.nl>
2020-04-06 07:48:19 +0200

    Standardize item permissions by moving codename to constant

--- backend/items/permissions.py
+++ backend/items/permissions.py
@@ -1,8 +1,15 @@
 from rest_framework import permissions
 from rdf.utils import create_custom_permissions
 
+'''
+Constant(s) with the code name for custom permissions.
+Note: if a permission needs to be passed to the frontend,
+do not forget to add this value to the UserDetailsSerializer in readit.serializers
+'''
+VIEW_ALL_ANNOTATIONS = 'view_all_annotations'
+
 custom_permissions = [
-    {'name': 'Can view all annotations', 'codename': 'view_all_annotations'}
+    {'name': 'Can view all annotations', 'codename': VIEW_ALL_ANNOTATIONS}
 ]
 
 '''
