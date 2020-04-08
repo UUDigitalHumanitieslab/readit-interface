03fe1c8034959dd1f0dac5e80c9a103af3a61804
Jelte van Boheemen <j.vanboheemen@uu.nl>
2020-04-08 16:57:19 +0200

    Query with SPARQL in GET request

--- backend/nlp_ontology/urls.py
+++ backend/nlp_ontology/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import ListNlpOntology
+from .views import NlpOntologyApiView
 
 urlpatterns = format_suffix_patterns([
-    path('', ListNlpOntology.as_view()),
+    path('', NlpOntologyApiView.as_view())
 ])
--- backend/nlp_ontology/views.py
+++ backend/nlp_ontology/views.py
@@ -1,9 +1,35 @@
-from rdf.views import RDFView
+import rdflib.plugins.sparql as rdf_sparql
+from pyparsing import ParseException
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_406_NOT_ACCEPTABLE
+from rest_framework.views import APIView
+
+from rdf.renderers import TurtleRenderer
+from rdf.utils import graph_from_triples
+
 from .graph import graph
 
 
-class ListNlpOntology(RDFView):
-    """ List the full ontology in RDF. """
+class NlpOntologyApiView(APIView):
+    """ Query or update the full ontology using SPARQL """
+    renderer_classes = (JSONRenderer, TurtleRenderer)
 
-    def graph(self):
-        return graph()
+    def get(self, request, **kwargs):
+        sparql = request.query_params.get('query') or None
+        if not sparql:
+            return Response(graph())
+
+        try:
+            query = rdf_sparql.prepareQuery(sparql)
+            query_results = graph().query(query)
+
+            a_r = request.accepted_renderer
+            if isinstance(a_r, TurtleRenderer):
+                return Response(graph_from_triples(query_results))
+            if isinstance(a_r, JSONRenderer):
+                return Response(query_results)
+            return Response('Accepted media types: application/json; text/turtle',
+                            status=HTTP_406_NOT_ACCEPTABLE)
+        except ParseException as p_e:
+            return Response("Invalid query {}: {}".format(sparql, p_e.msg), status=HTTP_400_BAD_REQUEST)
