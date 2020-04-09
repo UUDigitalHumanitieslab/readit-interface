65bcf5caddafca557ea2426a63939d6088ddf958
Jelte van Boheemen <j.vanboheemen@uu.nl>
2020-04-09 10:28:00 +0200

    Accept only update queries in POST

--- backend/nlp_ontology/views.py
+++ backend/nlp_ontology/views.py
@@ -12,12 +12,12 @@ from .graph import graph
 
 
 def render_query_results(query_results, accepted_renderer=None):
-    """ Render results bases on 'Accept' header: application/json (default) or txt/turtle """
+    """ Render queryresults based on 'Accept': application/json (default) or txt/turtle"""
     if isinstance(accepted_renderer, TurtleRenderer):
         return Response(graph_from_triples(query_results))
     if isinstance(accepted_renderer, JSONRenderer):
         return Response(query_results)
-    return Response('Accepted media types: application/json; text/turtle',
+    return Response('Accepted media types: application/json; txt/turtle',
                     status=HTTP_406_NOT_ACCEPTABLE)
 
 
@@ -27,28 +27,28 @@ class NlpOntologyApiView(APIView):
 
     def get(self, request, **kwargs):
         sparql = request.query_params.get('query')
+
         if not sparql:
             # GET without parameters: full ontology
+            # Ignores 'Accept' header, only renders txt/turtle
+            # TODO: see if this can be serialized to JSON
+            request.accepted_renderer = TurtleRenderer()
             return Response(graph())
+
         try:
             query = rdf_sparql.prepareQuery(sparql)
-            query_results = graph().query(query)
-            return render_query_results(query_results, request.accepted_renderer)
         except ParseException as p_e:
-            return Response("Invalid query {}: {}".format(sparql, p_e.msg), status=HTTP_400_BAD_REQUEST)
+            return Response("Invalid query {}: {}".format(sparql, p_e.msg),
+                            status=HTTP_400_BAD_REQUEST)
+
+        query_results = graph().query(query)
+        return render_query_results(query_results, request.accepted_renderer)
 
     def post(self, request, **kwargs):
         sparql = request.data.get('query')
         if not sparql:
-            return Response("Invalid query {}".format(sparql), status=HTTP_400_BAD_REQUEST)
-
-        try:
-            # try if query is a valid operation for this query and succeeds
-            query = rdf_sparql.prepareQuery(sparql)
-            query_results = graph().query(query)
-            return render_query_results(query_results, request.accepted_renderer)
-        except ParseException as p_e:
-            return Response("Invalid query {}: {}".format(sparql, p_e.msg), status=HTTP_400_BAD_REQUEST)
+            return Response("No SPARQL query provided in request body",
+                            status=HTTP_400_BAD_REQUEST)
 
         try:
             # try if update is a valid operation for this query and succeeds
@@ -56,8 +56,10 @@ class NlpOntologyApiView(APIView):
             query_results = graph().update(query)
             return render_query_results(query_results, request.accepted_renderer)
         except ParseException as p_e:
-            return Response("Invalid query {}: {}".format(sparql, p_e.msg), status=HTTP_400_BAD_REQUEST)
+            return Response("Invalid SPARQL query {}: {}".format(sparql, p_e.msg),
+                            status=HTTP_400_BAD_REQUEST)
         except TypeError as t_e:
-            return Response("Invalid query {}: {}".format(sparql, t_e), status=HTTP_400_BAD_REQUEST)
-
-        return Response('heeeee')
+            return Response(
+                "Not a valid query for UDPATE operation: {}, msg: {}".format(
+                    sparql, t_e),
+                status=HTTP_400_BAD_REQUEST)
