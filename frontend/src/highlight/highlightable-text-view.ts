ac74e77bc47880b777b661aface5cd1db9a79447
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-04-23 13:02:24 +0200

    Go for the normalize option after all (#283, #285)
    The two alternative solutions turned out to be not entirely
    equivalent. The old method of inserting the text with a template
    string treats \r\n as a single character while .text() treats it as
    two. This may cause pre-existing highlights to appear misaligned if
    the source contains CRLF sequences. The former method is also more in
    agreement with Python's way of decoding to str. I did however discover
    that the template string approach trims leading newlines and possibly
    other whitespace.
    
    @JeltevanBoheemen FYI.

--- frontend/src/highlight/highlightable-text-view.ts
+++ frontend/src/highlight/highlightable-text-view.ts
@@ -142,10 +142,11 @@ export default class HighlightableTextView extends View {
     onReady(): this {
         const text = this.text.responseText;
         this.isInDOM = true;
-        this.textWrapper = $('<pre class="textWrapper">').text(text);
+        this.textWrapper = $(`<pre class="textWrapper">${text}</pre>`);
         this.positionContainer = $('<div class="position-container">');
         this.textWrapper.appendTo(this.positionContainer);
         this.positionContainer.appendTo(this.$el);
+        this.textWrapper.get(0).normalize();
         this.listenTo(this.collection, 'add', this.addHighlight);
         this.listenTo(this.collection, 'update', this.onHighlightViewsUpdated);
         this.listenTo(this.collection, 'remove', this.destroyNode)
