9cdbb0fce3bfb84234d38d13e166757843a12e59
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-05-13 16:01:42 +0200

    Fix an oversight in the SourcePanelView tests (#274)

--- frontend/src/panel-source/source-view-test.ts
+++ frontend/src/panel-source/source-view-test.ts
@@ -1,5 +1,6 @@
 import { $ } from 'backbone';
 
+import { startStore, endStore } from '../test-util';
 import mockSources from '../mock-data/mock-sources';
 
 import { schema, vocab } from '../jsonld/ns';
@@ -11,11 +12,13 @@ import SourcePanel from './source-view';
 const textURI = 'http://test.test/test.txt';
 
 describe('SourceView', function() {
+    beforeEach(startStore);
     beforeEach(function() {
         this.source = new Node(mockSources[0]);
         this.items = new Graph();
         this.flat = new FlatCollection(this.items);
     });
+    afterEach(endStore);
 
     it('can be constructed in isolation', function() {
         const view = new SourcePanel({
@@ -37,42 +40,40 @@ describe('SourceView', function() {
         this.source.unset(schema.text).set(vocab('fullText'), textURI);
 
         // This will cause the source panel to request the text with an XHR.
-        jasmine.Ajax.withMock(() => {
-            const view = new SourcePanel({
-                model: this.source,
-                collection: this.flat,
-            });
-            // No text, no highlightable text view.
-            expect(view.htv).not.toBeDefined();
-
-            // First required trigger: text is available.
-            jasmine.Ajax.requests.mostRecent().respondWith({
-                status: 200,
-                contentType: 'text/plain',
-                responseText: text,
-            });
-            // XHR response is handled async, but since the response itself was
-            // produced sync, the callback is already queued at this point. Just
-            // a small delay (5 ms below) is enough to ensure that the next
-            // block of code runs after it.
-            setTimeout(() => {
-
-                // The presence of the highlightable text view by itself only
-                // depends on the text and not on the other triggers.
-                expect(view.htv).toBeDefined();
-                expect(view.htv.highlightLayer).toBeDefined();
-
-                // Second required trigger: all annotations are complete.
-                this.flat.trigger('complete:all');
-                expect(view.htv.highlightLayer.$el.children().length).toBe(0);
-
-                // Third requried trigger: view is attached to the `document`.
-                $('body').append(view.el);
-                view.activate();
-                expect(view.htv.highlightLayer.$el.children().length).toBe(1);
-                view.remove();
-                done();
-            }, 5);
+        const view = new SourcePanel({
+            model: this.source,
+            collection: this.flat,
         });
+        // No text, no highlightable text view.
+        expect(view.htv).not.toBeDefined();
+
+        // First required trigger: text is available.
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            status: 200,
+            contentType: 'text/plain',
+            responseText: text,
+        });
+        // XHR response is handled async, but since the response itself was
+        // produced sync, the callback is already queued at this point. Just
+        // a small delay (5 ms below) is enough to ensure that the next
+        // block of code runs after it.
+        setTimeout(() => {
+
+            // The presence of the highlightable text view by itself only
+            // depends on the text and not on the other triggers.
+            expect(view.htv).toBeDefined();
+            expect(view.htv.highlightLayer).toBeDefined();
+
+            // Second required trigger: all annotations are complete.
+            this.flat.trigger('complete:all');
+            expect(view.htv.highlightLayer.$el.children().length).toBe(0);
+
+            // Third requried trigger: view is attached to the `document`.
+            $('body').append(view.el);
+            view.activate();
+            expect(view.htv.highlightLayer.$el.children().length).toBe(1);
+            view.remove();
+            done();
+        }, 5);
     });
 });
