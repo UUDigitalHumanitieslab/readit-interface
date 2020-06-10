d75142d604b23cff5dce12ad0c0dcdc704cff57e
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-06-10 17:26:44 +0200

    Add the exploration router as specified in #62

---
+++ frontend/src/navigation/exploration-router.ts
@@ -0,0 +1,17 @@
+import { extend } from 'lodash';
+
+import Router from '../core/router';
+
+export default class ExplorationRouter extends Router {}
+
+extend(ExplorationRouter.prototype, {
+    routes: {
+        'explore/source/:serial':             'source',
+        'explore/source/:serial/annotations': 'source:annotated',
+        'explore/item/:serial':               'item',
+        'explore/item/:serial/edit':          'item:edit',
+        'explore/item/:serial/related':       'item:related',
+        'explore/item/:serial/related/edit':  'item:related:edit',
+        'explore/item/:serial/annotations':   'item:annotations',
+    },
+});
