77113e4d86b0abb1e7841a3471f37ac926c295c4
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-07-07 14:16:33 +0200

    Add a way to obtain the current user URI without breaking isolation

--- frontend/src/global/user.ts
+++ frontend/src/global/user.ts
@@ -1,3 +1,14 @@
 import User from '../user/user-model';
+import ldChannel from '../jsonld/radio';
+import { staff } from '../jsonld/ns';
 
-export default new User();
+const user = new User();
+
+function getUserURI() {
+    const username = user.get('username');
+    if (username) return staff(username);
+}
+
+ldChannel.reply('current-user-uri', getUserURI);
+
+export default user;
