0c38d274b23d47dad2f3303b6d5420d92ea818db
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-07-28 16:23:09 +0200

    Factor out the credentials from the auth_client fixture (#326)

--- backend/conftest.py
+++ backend/conftest.py
@@ -2,9 +2,13 @@ import pytest
 
 
 @pytest.fixture
-def auth_client(client, django_user_model):
-    username = 'tester'
-    password = 'testing123'
+def credentials():
+    return 'tester', 'testing123'
+
+
+@pytest.fixture
+def auth_client(client, django_user_model, credentials):
+    username, password = credentials
     django_user_model.objects.create_user(username=username, password=password)
     client.login(username=username, password=password)
     yield client
