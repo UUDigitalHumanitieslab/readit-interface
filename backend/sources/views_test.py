881f847c915ca3bdcf2b976c0754a988ec3e2d84
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-07-30 13:22:24 +0200

    Add a test to reproduce the ineffective source deletion bug (#326)
    Unfortunately, this didn't actually reproduce the bug; the test
    passed.

--- backend/sources/views_test.py
+++ backend/sources/views_test.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from rdf.ns import *
 
 from . import namespace as my
@@ -11,3 +13,17 @@ def test_delete_source_unauthorized(auth_client):
     response = auth_client.delete('/source/1')
     assert response.status_code == 403
     assert test_triple in sources
+
+
+def test_delete_source(auth_client, credentials):
+    name, pwd = credentials
+    User = get_user_model()
+    user = User.objects.get(username=name)
+    user.is_superuser = True
+    user.save()
+    test_triple = (my['1'], OWL.sameAs, my['1'])
+    sources = graph()
+    sources.add(test_triple)
+    response = auth_client.delete('/source/1')
+    assert response.status_code == 200
+    assert test_triple not in sources
