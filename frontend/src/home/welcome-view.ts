493ce0b6eea9cec871782700b5965114c50ae788
BeritJanssen <berit.janssen@gmail.com>
2020-08-10 09:43:24 +0200

    call to backend

--- frontend/src/home/welcome-view.ts
+++ frontend/src/home/welcome-view.ts
@@ -1,6 +1,7 @@
 import { extend } from 'lodash';
 import Model from '../core/model';
 import View from '../core/view';
+import Graph from '../jsonld/graph';
 import { ViewOptions as BaseOpt } from 'backbone';
 import welcomeTemplate from './welcome-template';
 
@@ -10,6 +11,7 @@ export interface ViewOptions extends BaseOpt<Model> {
 
 export default class WelcomeView extends View {
     searchboxView: View;
+    sources: SearchResult[];
 
     constructor(options: ViewOptions) {
         super(options);
@@ -19,16 +21,17 @@ export default class WelcomeView extends View {
     render() {
         this.$el.html(this.template(this));
 
-        this.$('.welcome-image').append(this.searchboxView.render().$el)
-        this.searchboxView.on("searchClicked", this.search)
+        this.$('.welcome-image').append(this.searchboxView.render().$el);
+        this.searchboxView.on("searchClicked", this.search);
 
         return this;
     }
 
     search(query: string, queryfields: string = 'all') {
-        // var url = encodeURI(`search/?query=${query}&queryfields=${queryfields}`);
-        // console.log('searching! (not implemented yet)');
-        alert('Sorry, not implemented yet!');
+        const sources = new Graph();
+        sources.fetch({ url: '/source/search/', data: $.param({ query: query, queryfields: queryfields})  }).then( bla => {
+            console.log('fetch!');
+        })
     }
 }
 
@@ -36,3 +39,11 @@ extend(WelcomeView.prototype, {
     tagName: 'section',
     template: welcomeTemplate
 });
+
+export type SearchResult = {
+    text: string,
+    language: string,
+    id: number,
+    score: number,
+
+}
