2332bfa54fb47d22faaaf5e4b69d8f0bc5c5c398
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-08-18 12:50:46 +0200

    Save the panel cid when navigating (#62, #106)

--- frontend/src/aspects/exploration.ts
+++ frontend/src/aspects/exploration.ts
@@ -4,4 +4,11 @@ import router from '../global/exploration-router';
 import explorer from '../global/explorer-view';
 import '../global/explorer-controller';
 
-channel.on('currentRoute', route => router.navigate(route));
+const browserHistory = window.history;
+
+channel.on('currentRoute', (route, panel) => {
+    router.navigate(route);
+    // Amend the state that Backbone.history just pushed with the cid of the
+    // panel.
+    browserHistory.replaceState(panel.cid, document.title);
+});
