091b28dae166be66882b00462352a09762c542fc
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-08-18 13:07:07 +0200

    Scroll to a previously visited panel on route (#62 #106)

--- frontend/src/aspects/exploration.ts
+++ frontend/src/aspects/exploration.ts
@@ -1,5 +1,6 @@
-import channel from '../explorer/radio';
+import { isString } from 'lodash';
 
+import channel from '../explorer/radio';
 import router from '../global/exploration-router';
 import explorer from '../global/explorer-view';
 import '../global/explorer-controller';
@@ -11,4 +12,10 @@ channel.on('currentRoute', (route, panel) => {
     // Amend the state that Backbone.history just pushed with the cid of the
     // panel.
     browserHistory.replaceState(panel.cid, document.title);
+});
+
+router.on('route', (route, params) => {
+    const state = browserHistory.state;
+    // state can only be a string if we made it so.
+    if (isString(state)) explorer.scroll(state);
 });
