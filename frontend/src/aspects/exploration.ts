d30ed09b191c18a2f1044c06370bf162f43652cb
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-08-20 16:20:54 +0200

    Replace route parser by a pattern that is easier on the brain (#62 #106)

--- frontend/src/aspects/exploration.ts
+++ frontend/src/aspects/exploration.ts
@@ -1,7 +1,7 @@
-import { isString } from 'lodash';
+import { partial, isString } from 'lodash';
 
 import channel from '../explorer/radio';
-import executeRoute from '../explorer/route-parser';
+import * as act from '../explorer/route-actions';
 import router from '../global/exploration-router';
 import mainRouter from '../global/main-router';
 import explorer from '../global/explorer-view';
@@ -48,7 +48,25 @@ mainRouter.on('route:explore', () => {
     );
 });
 
-router.on('route', (route, [serial]) => explorer.scrollOrAction(
-    browserHistory.state,
-    () => executeRoute(route, controller, serial)
-));
+/**
+ * Common patterns for the explorer routes.
+ */
+function deepRoute(obtainAction, resetAction) {
+    return ([serial]) => explorer.scrollOrAction(
+        browserHistory.state,
+        () => resetAction(controller, obtainAction(serial))
+    );
+}
+
+const sourceRoute = partial(deepRoute, act.getSource);
+const itemRoute = partial(deepRoute, act.getItem);
+
+router.on('route:source:bare',       sourceRoute(act.sourceWithoutAnnotations));
+router.on('route:source:annotated',  sourceRoute(act.sourceWithAnnotations));
+router.on('route:item',                itemRoute(act.item));
+router.on('route:item:edit',           itemRoute(act.itemInEditMode));
+router.on('route:item:related',        itemRoute(act.itemWithRelations));
+router.on('route:item:related:edit',   itemRoute(act.itemWithEditRelations));
+router.on('route:item:external',       itemRoute(act.itemWithExternal));
+router.on('route:item:external:edit',  itemRoute(act.itemWithEditExternal));
+router.on('route:item:annotations',    itemRoute(act.itemWithOccurrences));
