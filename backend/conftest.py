5d5ffc6848e1509a4920a4b8a9dc0fc81ab84d1d
Jelte van Boheemen <j.vanboheemen@uu.nl>
2020-09-08 15:26:01 +0200

    Test revisions

--- backend/conftest.py
+++ backend/conftest.py
@@ -27,6 +27,7 @@ def auth_client(client, django_user_model, credentials):
 @pytest.fixture
 def sparqlstore(settings):
     store = settings.RDFLIB_STORE
+    store.update('CLEAR ALL')
     assert not store.query(HAS_TRIPLES)
     yield store
     store.update('CLEAR ALL')
