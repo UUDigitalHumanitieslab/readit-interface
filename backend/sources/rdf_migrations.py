2a498c596eb1d9ec455f082ca399346f99230d09
Jelte van Boheemen <j.vanboheemen@uu.nl>
2020-10-07 10:25:25 +0200

    Remove redundant schema:creator.
    Replace with schema.author where necessary. Resolves #373.

--- backend/sources/rdf_migrations.py
+++ backend/sources/rdf_migrations.py
@@ -1,7 +1,27 @@
-from rdf.migrations import RDFMigration
+from rdf.migrations import RDFMigration, on_remove
 from .graph import graph
 from .fixture import canonical_graph
+from rdf.ns import SCHEMA
+
+REPLACE_CREATOR_UPDATE = '''
+DELETE {
+    ?s ?creator ?o .
+}
+INSERT {
+    ?s ?author ?o .
+}
+WHERE {
+    ?s ?creator ?o .
+}
+'''
+
 
 class Migration(RDFMigration):
     actual = staticmethod(graph)
     desired = staticmethod(canonical_graph)
+
+    @on_remove(SCHEMA.creator)
+    def replace_SCHEMA_creator(self, actual, conjunctive):
+        graph().update(REPLACE_CREATOR_UPDATE,
+                       initBindings={'creator': SCHEMA.creator,
+                                     'author': SCHEMA.author})
