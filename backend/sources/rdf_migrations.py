922313c477eb988f3cfbcf42d7764ec2da702c4d
Jelte van Boheemen <j.vanboheemen@uu.nl>
2020-10-07 13:19:30 +0200

    Add migration handler for present predicate

--- backend/sources/rdf_migrations.py
+++ backend/sources/rdf_migrations.py
@@ -1,4 +1,4 @@
-from rdf.migrations import RDFMigration, on_remove
+from rdf.migrations import RDFMigration, on_present
 from .graph import graph
 from .fixture import canonical_graph
 from rdf.ns import SCHEMA
@@ -20,8 +20,8 @@ class Migration(RDFMigration):
     actual = staticmethod(graph)
     desired = staticmethod(canonical_graph)
 
-    @on_remove(SCHEMA.creator)
-    def replace_SCHEMA_creator(self, actual, conjunctive):
+    @on_present(SCHEMA.author)
+    def test_add(self, actual, conjunctive):
         graph().update(REPLACE_CREATOR_UPDATE,
                        initBindings={'creator': SCHEMA.creator,
                                      'author': SCHEMA.author})
