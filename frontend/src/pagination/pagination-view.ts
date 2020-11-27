4399dd34282dd26c5258b7ec0bfcb6f6e6be5d13
BeritJanssen <berit.janssen@gmail.com>
2020-11-27 15:35:19 +0100

    used icons rather than text for previous / next, boolean for determining first / last page button, added comments

--- frontend/src/pagination/pagination-view.ts
+++ frontend/src/pagination/pagination-view.ts
@@ -15,14 +15,18 @@ export default class PaginationView extends View {
     pageCenter: number;
     pagePlus: number;
     pageMinus: number;
+    showFirstLastPageLink: boolean;
 
     initialize(options: ViewOptions) {
         this.totalPages = options.totalPages;
+        // show the first and last page link only if we have more than three pages
+        this.showFirstLastPageLink = this.totalPages > 3 ? true : false;
         this.determineCurrentPages(1);
     }
 
     render(): this {
         this.$el.html(this.template(this));
+        // add is-current class to left, center or right pagination link
         if (this.currentPage === this.pagePlus) {
             this.$('#page-plus').addClass('is-current');
         } else if (this.currentPage === this.pageCenter) {
@@ -30,6 +34,7 @@ export default class PaginationView extends View {
         } else if (this.currentPage === this.pageMinus) {
             this.$('#page-minus').addClass('is-current');
         }
+        //disable previous and next buttons if we are on first or last page
         if (this.currentPage === 1) {
             this.$('.pagination-previous').attr('disabled', 'true');
         } else if (this.currentPage === this.totalPages) {
@@ -52,8 +57,17 @@ export default class PaginationView extends View {
         this.currentPage = page;
         this.showPaginationLinks = this.initializePaginationLinks();
         if (this.totalPages < 3) {
-            this.pageMinus = page;
-            this.pageCenter = 2;
+            // special case: only 2 pages
+            // pageMinus is the left page link, which is always one or two
+            // pagePlus is the right page link, only shown for three and more pages
+            if (page == 1 ) {
+                this.pageMinus = page;
+                this.pageCenter = page+1;
+            }
+            else {
+                this.pageMinus = page-1;
+                this.pageCenter = page;
+            }
         } else {
             this.adjustPaginationLinks(page);
             if (page === 1) {
@@ -74,10 +88,12 @@ export default class PaginationView extends View {
     }
 
     adjustPaginationLinks(page: number) {
+        // control visiblity of first / last page link
+        // control visiblity of ellipses at start and end
         this.showPaginationLinks['pagePlus'] = true;
-        if (this.totalPages > 3 && page > 2) this.showPaginationLinks['start'] = true;
+        if (this.showFirstLastPageLink && page > 2) this.showPaginationLinks['start'] = true;
         if (page > 3) this.showPaginationLinks['ellipsisStart'] = true;
-        if (this.totalPages > 3 && page < this.totalPages-1) this.showPaginationLinks['end'] = true;
+        if (this.showFirstLastPageLink && page < this.totalPages-1) this.showPaginationLinks['end'] = true;
         if (page < this.totalPages-2) this.showPaginationLinks['ellipsisEnd'] = true;
     }
 
