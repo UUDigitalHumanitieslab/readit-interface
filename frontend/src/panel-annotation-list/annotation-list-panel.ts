92a75ae2005d5a5dd5bc70b30d72071a64eadd14
BeritJanssen <berit.janssen@gmail.com>
2021-03-01 14:46:34 +0100

    removing panel-header class

--- frontend/src/panel-annotation-list/annotation-list-panel.ts
+++ frontend/src/panel-annotation-list/annotation-list-panel.ts
@@ -4,26 +4,21 @@ import { baseUrl } from 'config.json';
 
 import explorerChannel from '../explorer/explorer-radio';
 import FlatItem from '../common-adapters/flat-item-model';
-import ItemGraph from '../common-adapters/item-graph';
 import { CompositeView } from '../core/view';
 import { announceRoute } from '../explorer/utilities';
-import { getScrollTop, animatedScroll, ScrollType } from '../utilities/scrolling-utilities';
 
 import annotationListPanelTemplate from './annotation-list-panel-template';
 import AnnotationListView from './annotation-list-view';
-import HeaderView from '../panel-header/panel-header-view';
 
 const itemUrl = baseUrl + 'item/'
 const announce = announceRoute('source:annotated', ['model', 'id']);
 
 export default class AnnotationListPanel extends CompositeView<FlatItem> {
-    header: HeaderView;
     annotationList: AnnotationListView;
     downloadLink: string;
 
     initialize(): void {
         this.downloadLink = itemUrl + 'download?o=' + this.model.id + '&t=1&r=1'; 
-        this.header = new HeaderView({ model: { title: 'Annotations' } });
         this.annotationList = new AnnotationListView({
             collection: this.collection,
             model: this.model
