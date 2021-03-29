7aac33f4ec5bcd3922098c127689484855c0a2fb
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-03-29 11:19:03 +0200

    Fix language tags, cleanup

--- backend/scripts/xml_sanitize.py
+++ backend/scripts/xml_sanitize.py
@@ -2,23 +2,23 @@ from rdflib import Literal
 from items.graph import graph as item_graph
 from sparql.utils import find_invalid_xml, xml_sanitize_triple
 from django.conf import settings
-from os import environ
+from os import environ, getcwd, path
 from lxml.etree import XMLSyntaxError
 environ.setdefault('DJANGO_SETTINGS_MODULE', 'readit.settings')
 
 
 settings.RDFLIB_STORE.returnFormat = 'json'
 
+dirty_triples_filepath = path.abspath(
+    path.join(getcwd(), '..', '..', 'dirty_triples.csv'))
+
 
 def find_dirty_triples():
-    try:
-        g = item_graph()
-        for triple in g:
-            is_cleaned, cleaned_triple = xml_sanitize_triple(triple)
-            if is_cleaned:
-                print(triple, cleaned_triple)
-    except XMLSyntaxError:
-        print("Cant parse graph, use resultFormat='json'")
+    g = item_graph()
+    for triple in g:
+        is_cleaned, cleaned_triple = xml_sanitize_triple(triple)
+        if is_cleaned:
+            print(triple, cleaned_triple)
 
 
 def clean_dirty_triples():
