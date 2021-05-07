4216154d9d3dc8cf3d0cb2db0c32d5fee5efc351
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-05-07 16:36:16 +0200

    Distinguish instance vs. @type usage of applicablePredicates (#455)

--- frontend/src/utilities/relation-utilities-test.ts
+++ frontend/src/utilities/relation-utilities-test.ts
@@ -2,9 +2,10 @@ import { constant } from 'lodash';
 
 import { startStore, endStore } from '../test-util';
 import mockOntology from '../mock-data/mock-ontology';
+import { anno2ReaderInstance } from '../mock-data/mock-items';
 
 import ldChannel from '../common-rdf/radio';
-import { readit, skos } from '../common-rdf/ns';
+import { readit, skos, rdfs } from '../common-rdf/ns';
 import Node from '../common-rdf/node';
 import Graph from '../common-rdf/graph';
 
@@ -25,12 +26,19 @@ describe('relation utilities', function() {
     afterEach(endStore);
 
     describe('applicablePredicates', function() {
-        it('returns the predicates applicable to a given class', function() {
-            const Reader = ldChannel.request('obtain', readit('Reader'));
-            const predicates = applicablePredicates(Reader);
+        it('returns the predicates applicable to a given item', function() {
+            const Blessington = new Node(anno2ReaderInstance);
+            const predicates = applicablePredicates(Blessington);
             expect(predicates.length).toBe(2);
-            expect(predicates.at(0).get(skos.prefLabel)).toBe('description of');
-            expect(predicates.at(1).get(skos.prefLabel)).toBe('inverse of description of');
+            expect(predicates.at(0).get(skos.prefLabel)[0]).toBe('description of');
+            expect(predicates.at(1).get(rdfs.label)[0]).toBe('inverse of description of');
+        });
+
+        it('returns the predicates applicable to a given @type', function() {
+            const predicates = applicablePredicates(readit('Reader'));
+            expect(predicates.length).toBe(2);
+            expect(predicates.at(0).get(skos.prefLabel)[0]).toBe('description of');
+            expect(predicates.at(1).get(rdfs.label)[0]).toBe('inverse of description of');
         });
     });
 });
