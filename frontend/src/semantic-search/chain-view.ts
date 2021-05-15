5193c7605acb0ae1f0d4d16011a09a7eba7c4ebe
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-05-15 19:26:52 +0200

    Obtain branchout view from semChannel (#455)

--- frontend/src/semantic-search/chain-view.ts
+++ frontend/src/semantic-search/chain-view.ts
@@ -3,7 +3,9 @@ import { extend } from 'lodash';
 import Model from '../core/model';
 import Collection from '../core/collection';
 import View, { CollectionView } from '../core/view';
+import { xsd } from '../common-rdf/ns';
 
+import semChannel from './radio';
 import Dropdown from './dropdown-view';
 
 export default class Chain extends CollectionView {
@@ -19,13 +21,11 @@ export default class Chain extends CollectionView {
     }
 
     makeItem(model: Model): View {
-        const precedent = model.get('precedent');
-        const scheme = precedent && precedent.id.split(':')[0];
-        switch (scheme) {
-        case 'logic':
-            // TODO
-        case 'filter':
-            // TODO
+        const action = model.get('action');
+        switch (action) {
+        case 'and':
+        case 'or':
+            return semChannel.request('branchout', model);
         }
         return new Dropdown({ model });
     }
@@ -33,7 +33,20 @@ export default class Chain extends CollectionView {
     updateControls(model, selection): void {
         const collection = this.collection;
         while (collection.last() !== model) collection.pop();
-        collection.push({ precedent: selection } as unknown as Model);
+        const precedent = model.get('precedent');
+        const [scheme, action] = selection ? selection.id.split(':') : [];
+        const newModel = new Model();
+        switch (scheme) {
+        case 'logic':
+            newModel.set({ precedent });
+            if (action !== 'not') newModel.set({ scheme, action });
+            break;
+        case 'filter':
+            // TODO
+        default:
+            newModel.set('precedent', selection);
+        }
+        collection.push(newModel);
     }
 }
 
