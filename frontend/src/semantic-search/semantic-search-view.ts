bcf8b981de9ca139949f73f9d6112ac499c6fef1
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-05-15 19:17:57 +0200

    Add the top-level semantic search view (#455)

---
+++ frontend/src/semantic-search/semantic-search-view.ts
@@ -0,0 +1,45 @@
+import { extend } from 'lodash';
+
+import Model from '../core/model';
+import { CompositeView } from '../core/view';
+import Multifield from '../forms/multifield-view';
+
+import semChannel from './radio';
+import Chain from './chain-view';
+import Multibranch from './multibranch-view';
+import semTemplate from './semantic-search-template';
+
+export default class SemanticSearchView extends CompositeView {
+    topChain: Chain;
+
+    initialize(): void {
+        this.topChain = new Chain({ model: this.model });
+        semChannel.reply('branchout', this.branchout, this);
+        this.render();
+    }
+
+    renderContainer(): this {
+        this.$el.html(this.template({}));
+        return this;
+    }
+
+    remove(): this {
+        semChannel.stopReplying('branchout', this.branchout);
+        return super.remove();
+    }
+
+    branchout(model: Model): Multifield {
+        const collectionView = new Multibranch({ model });
+        return new Multifield({ collectionView });
+    }
+}
+
+extend(SemanticSearchView.prototype, {
+    className: 'rit-semantic-search',
+    template: semTemplate,
+    subviews: [{
+        view: 'topChain',
+        method: 'after',
+        selector: '.subtitle',
+    }],
+});
