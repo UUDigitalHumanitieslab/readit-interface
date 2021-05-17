39178a67a4578b763e8fec65b6e89e26b37d6db6
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-05-17 16:51:45 +0200

    Use composition instead of inheritance for MultibranchRow (#455)

--- frontend/src/semantic-search/multibranch-row-view.ts
+++ frontend/src/semantic-search/multibranch-row-view.ts
@@ -1,34 +1,45 @@
+import { extend } from 'lodash';
+
+import { CompositeView } from '../core/view';
 import RemoveButton from '../forms/remove-button-view';
 
 import Chain from './chain-view';
+import multibranchRowTemplate from './multibranch-row-template';
 
 /**
- * Every row in a Multibranch is a Chain with a RemoveButton in front.
+ * Every row in a Multibranch has a Chain with a RemoveButton in front.
  */
-export default class MultibranchRow extends Chain {
+export default class MultibranchRow extends CompositeView {
     removeButton: RemoveButton;
+    chain: Chain;
 
     initialize(): void {
         this.removeButton = new RemoveButton().on('click', this.close, this);
-        super.initialize();
+        this.chain = new Chain({
+            model: this.model,
+            collection: this.collection,
+        }).on('all', this.trigger, this);
+        this.render();
     }
 
-    beforeRender(): this {
-        this.removeButton.$el.detach();
+    renderContainer(): this {
+        this.$el.html(this.template({}));
         return this;
-    }
-
-    afterRender(): this {
-        this.removeButton.$el.prependTo(this.$el);
-        return this;
-    }
-
-    remove(): this {
-        this.removeButton.remove();
-        return super.remove();
     }
 
     close(): void {
         this.trigger('close', this, this.model);
     }
 }
+
+extend(MultibranchRow.prototype, {
+    className: 'field is-horizontal',
+    template: multibranchRowTemplate,
+    subviews: [{
+        view: 'removeButton',
+        selector: '.field-label',
+    }, {
+        view: 'chain',
+        selector: '.field-body',
+    }],
+});
