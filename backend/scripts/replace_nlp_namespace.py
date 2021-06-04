fec524c8feb390ff84b7dabfafa632e7dfcb9eaa
BeritJanssen <berit.janssen@gmail.com>
2021-06-04 11:07:05 +0200

    script to replace nlp namespace

---
+++ backend/scripts/replace_nlp_namespace.py
@@ -0,0 +1,23 @@
+"""
+Script for replacing nlp namespace
+
+Usage: open an interactive Python shell with Django's `shell`
+command. When working on a server, pass the arguments `--settings
+settings --pythonpath {directory/of/settings/file}`.
+Then:
+>>> from scripts.replace_nlp_namespace import replace_nlp_namespace
+>>> replace_nlp_namespace()
+"""
+
+if __name__ == '__main__':
+    import sys
+    print(__doc__)
+    sys.exit()
+
+
+from ontology.fixture import replace_prefix
+from sources.constants import NLP_NS, INSTANCE_NLP_NS
+
+def replace_nlp_namespace():
+    g = Graph(settings.RDFLIB_STORE, NLP_ONTOLOGY_NS)
+    replace_prefix(g, NLP_NS, INSTANCE_NLP_NS)
