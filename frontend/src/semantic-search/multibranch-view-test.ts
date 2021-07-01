8d0a2c720977befa60ec02c747050736f728c40e
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-07-01 14:08:24 +0200

    Clean up test views at the end of each spec
    A leftover select2 widget was overlaid on the Jasmine browser
    interface because I didn't do this originally.

--- frontend/src/semantic-search/multibranch-view-test.ts
+++ frontend/src/semantic-search/multibranch-view-test.ts
@@ -4,5 +4,6 @@ describe('semantic search MultibranchView', function() {
     it('can be constructed in isolation', function() {
         const view = new Multibranch();
         expect(view.$el.children().length).toBe(1);
+        view.remove();
     });
 });
