4d53ed1525475b5b9a4e7922e884958dfa3c9645
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-07-01 17:43:35 +0200

    Add backend model for storing semantic queries in JSON form (#486)

--- backend/items/models.py
+++ backend/items/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+
 from rdf.baseclasses import BaseCounter
 from .constants import ITEMS_NS, ITEMS_HISTORY_NS
 
@@ -8,3 +11,11 @@ class ItemCounter(BaseCounter):
 
 class EditCounter(BaseCounter):
     namespace = ITEMS_HISTORY_NS
+
+
+class SemanticQuery(models.Model):
+    label = models.CharField(blank=True, max_length=100)
+    query = JSONField()
+
+    def __str__(self):
+        return self.label or str(self.id)
