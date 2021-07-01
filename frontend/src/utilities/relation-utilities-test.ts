e573e260910db1e4bc349e1cc6f966a233d6597b
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-07-01 13:40:33 +0200

    Add a test that exposes the missing inverse property label bug

--- frontend/src/utilities/relation-utilities-test.ts
+++ frontend/src/utilities/relation-utilities-test.ts
@@ -1,6 +1,6 @@
 import { constant } from 'lodash';
 
-import { startStore, endStore } from '../test-util';
+import { startStore, endStore, event } from '../test-util';
 import mockOntology from '../mock-data/mock-ontology';
 import { anno2ReaderInstance } from '../mock-data/mock-items';
 
@@ -8,6 +8,7 @@ import ldChannel from '../common-rdf/radio';
 import { readit, skos, rdfs } from '../common-rdf/ns';
 import Node from '../common-rdf/node';
 import Graph from '../common-rdf/graph';
+import FlatItem from '../common-adapters/flat-item-model';
 
 import { applicablePredicates } from './relation-utilities';
 
@@ -39,6 +40,13 @@ describe('relation utilities', function() {
             expect(predicates.length).toBe(2);
             expect(predicates.at(0).get(skos.prefLabel)[0]).toBe('description of');
             expect(predicates.at(1).get(rdfs.label)[0]).toBe('inverse of description of');
+        });
+
+        it('generates inverse properties that can be flattened', async function() {
+            const inverse = applicablePredicates(readit('Reader')).at(1);
+            const flat = new FlatItem(inverse);
+            await event(flat, 'complete');
+            expect(flat.get('classLabel')).toBe('inverse of description of');
         });
     });
 });
