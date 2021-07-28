831a98378108278c99209ceb955731b89bebd094
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-07-28 16:58:06 +0200

    Automatically set the creator on newly posted semantic queries (#486)

--- backend/items/serializers.py
+++ backend/items/serializers.py
@@ -7,3 +7,7 @@ class SemanticQuerySerializer(serializers.ModelSerializer):
     class Meta:
         model = SemanticQuery
         fields = ['id', 'label', 'query']
+
+    def create(self, validated_data):
+        validated_data['creator'] = self.context['request'].user
+        return super().create(validated_data)
