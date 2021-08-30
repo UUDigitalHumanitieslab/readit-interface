d7703fc68af7df12123fad062277a4cfc0f1a5dd
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-08-30 17:03:02 +0200

    Prevent aliasing of the options object in Node response cycle (#55 #492)

--- frontend/src/core/csrf.ts
+++ frontend/src/core/csrf.ts
@@ -1,4 +1,4 @@
-import { defaultsDeep, identity as id, overArgs } from 'lodash';
+import { extend, identity as id, overArgs } from 'lodash';
 import { sync } from 'backbone';
 import * as Cookies from 'js-cookie';
 
@@ -7,8 +7,7 @@ const sameOrigin = RegExp(window.location.host);
 
 /**
  * Insert the CSRF token header into $.ajax-compatible request options.
- * Returns a new object, does not mutate the original object.
- * Returns the same object unchanged if the request is nonmodifying or
+ * Returns the object unchanged if the request is nonmodifying or
  * to another host.
  */
 export function addCSRFToken(ajaxOptions) {
@@ -17,9 +16,11 @@ export function addCSRFToken(ajaxOptions) {
     if (url && nonRelative.test(url) && !sameOrigin.test(url)) {
         return ajaxOptions;
     }
-    return defaultsDeep({
-        headers: {'X-CSRFToken': Cookies.get('csrftoken')},
-    }, ajaxOptions);
+    const headers = ajaxOptions.headers || {};
+    ajaxOptions.headers = extend(headers, {
+        'X-CSRFToken': Cookies.get('csrftoken'),
+    });
+    return ajaxOptions;
 }
 
 /**
