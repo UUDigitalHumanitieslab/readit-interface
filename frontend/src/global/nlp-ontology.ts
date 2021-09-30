cae0934d743ecfd402c89408454c7c8410afe5a9
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-09-30 15:05:01 +0200

    Give global NLP ontology same treatment as other ontology (#180)
    "Same" as in 50bdea25, 99e18c08, 29a3e65b, 65f9e583, fb2c467f.

--- frontend/src/global/nlp-ontology.ts
+++ frontend/src/global/nlp-ontology.ts
@@ -1,5 +1,11 @@
 /**
- * This module provides its service through one trigger and two
+ * This module provides global access to the ontology. While the
+ * ontology is provided as a default export, this is only meant for
+ * other global and aspect modules; unit modules should only access
+ * the ontology indirectly through the linked data radio channel. The
+ * ontology is fetched lazily, i.e., not before it is first requested.
+ *
+ * This module provides its service through one trigger and five
  * requests:
 
     ldChannel.trigger('cache:nlp-ontology')
@@ -22,38 +28,66 @@
  * sync code that can wait until a later event in order to access the
  * ontology Nodes, or in sync code where it is reasonable to assume
  * that the ontology has already been fetched.
+
+    ldChannel.request('nlp-ontology:colored')
+
+ * Like the previous, but returns a FilteredCollection with only the
+ * colored classes in the ontology.
+
+    ldChannel.request('nlp-ontology:flatColored')
+
+ * Like the previous, but with all colored classes represented as flat items.
+
+    ldChannel.request('nlp-ontology:hierarchy')
+
+ * Like the previous, but converted to a model hierarchy according to the
+ * convention described in ../hierarchy/hierarchy-view. This replies with a
+ * promise to the hierarchy, rather than the hierarchy itself.
  */
 
+import { constant } from 'lodash';
+
+import Collection from '../core/collection';
 import ldChannel from '../common-rdf/radio';
 import { nlp } from '../common-rdf/ns';
+import Node from '../common-rdf/node';
 import Graph from '../common-rdf/graph';
+import FilteredCollection from '../common-adapters/filtered-collection';
+import FlatItemCollection from '../common-adapters/flat-item-collection';
+import { hierarchyFromNLPOntology } from '../hierarchy/ontology';
+import { isColoredClass } from '../utilities/linked-data-utilities';
 
 const nlpOntology = new Graph();
 export default nlpOntology;
 let promise: PromiseLike<Graph> = null;
+export const coloredClasses = new FilteredCollection<Node, Graph>(
+    nlpOntology, isColoredClass
+);
+export const flatColored = new FlatItemCollection(coloredClasses);
+const coloredComplete = new Promise(
+    resolve => flatColored.once('complete:all', resolve)
+);
+let hierarchy: PromiseLike<Collection> = null;
 
 /**
  * The function that takes care of the lazy fetching.
  */
 function ensurePromise(): PromiseLike<Graph> {
     if (promise) return promise;
-    promise = nlpOntology.fetch({ url: nlp() }).then(handleSuccess, handleError);
+    promise = nlpOntology.fetch({ url: nlp() }).then(constant(nlpOntology));
     return promise;
 }
 
 /**
- * Promise resolution and rejection handlers.
- * Besides returning the result or error, they short-circuit the
- * promise in order to save a few ticks.
+ * Take care of having a hierarchy of the colored classes.
  */
-function handleSuccess(): Graph {
-    promise = Promise.resolve(nlpOntology);
-    return nlpOntology;
-}
-
-function handleError(error: any): any {
-    promise = Promise.reject(error);
-    return error;
+function ensureHierarchy(): PromiseLike<Collection> {
+    hierarchy = hierarchy || Promise.all([
+        ensurePromise(), coloredComplete
+    ]).then(
+        () => hierarchyFromNLPOntology(flatColored)
+    );
+    return hierarchy;
 }
 
 /**
@@ -62,3 +96,6 @@ function handleError(error: any): any {
 ldChannel.once('cache:nlp-ontology', ensurePromise);
 ldChannel.reply('nlp-ontology:promise', ensurePromise);
 ldChannel.reply('nlp-ontology:graph', () => (ensurePromise(), nlpOntology));
+ldChannel.reply('nlp-ontology:colored', () => (ensurePromise(), coloredClasses));
+ldChannel.reply('nlp-ontology:flatColored', () => (ensurePromise(), flatColored));
+ldChannel.reply('nlp-ontology:hierarchy', ensureHierarchy);
