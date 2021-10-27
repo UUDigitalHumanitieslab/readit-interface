4b3e4fdc773a28a9b9ee3c707b66d07feff7388e
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-10-27 15:55:11 +0200

    Spam the backend with logging statements for debugging purposes

--- backend/nlp_ontology/graph.py
+++ backend/nlp_ontology/graph.py
@@ -1,16 +1,23 @@
+import logging
+
 from django.conf import settings
 from rdflib import Graph
 
 from ontology.fixture import replace_prefix
 from .constants import NLP_ONTOLOGY_NS, NLP_NS, INSTANCE_NLP_NS
 
+logger = logging.getLogger(__name__)
+
 
 def graph():
+    logger.debug('computing graph in nlp_ontology.graph')
     if settings.DEBUG == True:
+        logger.debug('constructing local testing graph')
         # return nlp ontology for local testing
         g = Graph()
         g.parse('nlp_ontology/nlp-ontology.trtl', format='turtle')
     else:
+        logger.debug('loading graph from SPARQL store')
         g = Graph(settings.RDFLIB_STORE, NLP_ONTOLOGY_NS)
     g = replace_prefix(g, NLP_NS, INSTANCE_NLP_NS)
     return g
