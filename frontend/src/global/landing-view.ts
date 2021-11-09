187849293dfd90f9b48f781b357490f9216fa899
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-11-09 17:39:05 +0100

    Reduce LandingView to a simple view of the global statistics model
    Ref #163 #354 #501

--- frontend/src/global/landing-view.ts
+++ frontend/src/global/landing-view.ts
@@ -1,3 +1,5 @@
 import LandingView from "../landing/landing-view";
+import ldChannel from '../common-rdf/radio';
+import './exploration-data';
 
-export default new LandingView();
+export default new LandingView({ model: ldChannel.request('statistics') });
--- frontend/src/landing/landing-view.ts
+++ frontend/src/landing/landing-view.ts
@@ -1,48 +1,20 @@
 import { extend } from 'lodash';
-import ItemGraph from '../common-adapters/item-graph';
 
 import View from "../core/view";
-import ldChannel from '../common-rdf/radio';
 import userChannel from '../common-user/user-radio';
 
 import landingTemplate from './landing-template';
 
 export default class LandingView extends View {
-    totalSources: number;
-    totalItems: number;
-    sources: ItemGraph;
-    items: ItemGraph;
-    userSources: number;
-    userItems: number;
-    user: string;
-
     initialize() {
-        userChannel.request('promise').then( (user) => {
-            this.user = user.get('username');
-        })
-        this.awaitNodeLists();
-        this.requestUserNodes();
+        this.render().listenTo(this.model, 'change', this.render);
     }
 
     render() {
-        this.$el.html(this.template(this));
+        const user = userChannel.request('user').get('username') || 'guest';
+        const data = extend({ user }, this.model.attributes);
+        this.$el.html(this.template(data));
         return this;
-    }
-
-    async awaitNodeLists() {
-        const itemList = await ldChannel.request('promise:item-list');
-        this.totalItems = itemList.length;
-        const sourceList = await ldChannel.request('promise:source-list');
-        this.totalSources = sourceList.length;
-        this.render();
-    }
-
-    async requestUserNodes() {
-        this.items = await ldChannel.request('promise:user-items');
-        this.userItems = this.items.length;
-        this.sources = await ldChannel.request('promise:user-sources');
-        this.userSources = this.sources.length;
-        this.render();
     }
 }
 
