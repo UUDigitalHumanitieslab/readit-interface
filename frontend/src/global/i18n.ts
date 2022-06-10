1d710752eb8b295a741379d1b511787fda4603c7
Julian Gonggrijp <dev@juliangonggrijp.com>
2022-06-10 16:45:03 +0200

    Expose i18nPromise through radio channel for access from views (#451)

--- frontend/src/global/i18n.ts
+++ frontend/src/global/i18n.ts
@@ -1,8 +1,10 @@
+import { constant } from 'lodash';
 import * as i18next from 'i18next';
 import * as languageDetector from 'i18next-browser-languagedetector';
 
 import * as english from '../i18n/en/translation.json';
 import * as french from '../i18n/fr/translation.json';
+import channel from '../i18n/radio';
 
 const i18nPromise = new Promise(function(resolve, reject) {
     i18next.use(
@@ -25,5 +27,7 @@ const i18nPromise = new Promise(function(resolve, reject) {
         }
     });
 });
+
+channel.reply('i18next', constant(i18nPromise));
 
 export { i18nPromise, i18next };
