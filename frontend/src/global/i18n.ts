08440c4640f98e47081392fc1e728a9c398c278d
Julian Gonggrijp <dev@juliangonggrijp.com>
2022-06-10 16:38:51 +0200

    Use native Promise for global/i18n (#451)

--- frontend/src/global/i18n.ts
+++ frontend/src/global/i18n.ts
@@ -1,31 +1,29 @@
-import { Deferred } from 'jquery';
 import * as i18next from 'i18next';
 import * as languageDetector from 'i18next-browser-languagedetector';
 
 import * as english from '../i18n/en/translation.json';
 import * as french from '../i18n/fr/translation.json';
 
-const deferred = Deferred();
-const i18nPromise = deferred.promise();
-
-i18next.use(
-    languageDetector
-).init({
-    fallbackLng: ['en', 'dev'],
-    resources: {
-        en: {
-            translation: english,
+const i18nPromise = new Promise(function(resolve, reject) {
+    i18next.use(
+        languageDetector
+    ).init({
+        fallbackLng: ['en', 'dev'],
+        resources: {
+            en: {
+                translation: english,
+            },
+            fr: {
+                translation: french,
+            },
         },
-        fr: {
-            translation: french,
-        },
-    },
-}, function(error, t) {
-    if (error) {
-        deferred.reject(error);
-    } else {
-        deferred.resolve(i18next);
-    }
+    }, function(error, t) {
+        if (error) {
+            reject(error);
+        } else {
+            resolve(i18next);
+        }
+    });
 });
 
 export { i18nPromise, i18next };
