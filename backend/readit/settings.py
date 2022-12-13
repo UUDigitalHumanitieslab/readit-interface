96a2cad5a64f718aa3c81f966d81907a034c17a7
Jelte van Boheemen <j.vanboheemen@uu.nl>
2022-12-13 12:48:39 +0100

    Add Brotli middleware to Django

--- backend/readit/settings.py
+++ backend/readit/settings.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'django_brotli.middleware.BrotliMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
