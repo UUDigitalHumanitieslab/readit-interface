54c7a90c13b0940faf5f81ecf879bf59c6fb7453
Julian Gonggrijp <j.gonggrijp@uu.nl>
2023-03-22 17:27:30 +0100

    Format dates in source metadata editing panel (#37 #517)

--- frontend/src/source-metadata/source-metadata-view.ts
+++ frontend/src/source-metadata/source-metadata-view.ts
@@ -164,6 +164,7 @@ export default class SourceMetadataView extends CompositeView {
                         ? nodeFromUri
                         : getLabel(nodeFromUri);
             }
+            if (value instanceof Date) value = value.toISOString();
             element.val(value);
         }
         return this;
