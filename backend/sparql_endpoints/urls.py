6c7003eeff079ea654a532ce083ee32aa4564457
lukavdplas <lukavdplas@gmail.com>
2023-05-04 11:42:28 +0200

    remove flatmap function

--- backend/sparql_endpoints/urls.py
+++ backend/sparql_endpoints/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from sparql_endpoints.views import sparql_query_view, sparql_update_view
 from sparql_endpoints.endpoints import SPARQL_ENDPOINTS
 from rest_framework.urlpatterns import format_suffix_patterns
-from typing import List, TypeVar, Callable
 
 def sparql_query_url(endpoint_setting):
     '''
@@ -36,22 +35,12 @@ def sparql_endpoint_urls(endpoint_setting):
     if endpoint_setting['enable_update']:
         yield sparql_update_url(endpoint_setting)
 
-T1 = TypeVar('T1')
-T2 = TypeVar('T2')
-
-def flatmap(func: Callable[[T1], List[T2]], collection: List[T1]) -> List[T2]:
-    '''
-    Apply `func` to each tiem in `collection` and flatten the result
-    into a single list.
-    '''
-    return [
-        output
-        for item in collection
-        for output in func(item)
-    ]
-
-'''Generator of all urls based on the SPARQL_ENDPOINTS setting.'''
-SPARQL_URLS = flatmap(sparql_endpoint_urls, SPARQL_ENDPOINTS)
+'''List of all urls based on the SPARQL_ENDPOINTS setting.'''
+SPARQL_URLS = [
+    url
+    for endpoint in SPARQL_ENDPOINTS
+    for url in sparql_endpoint_urls(endpoint)
+]
 
 urlpatterns = format_suffix_patterns(SPARQL_URLS)
 
