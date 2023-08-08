1ea35ad62b125b5f283960cdf3d3871dcc5e89a9
Julian Gonggrijp <j.gonggrijp@uu.nl>
2023-08-09 00:34:17 +0200

    Use Backbone.history "notfound" event from our Backbone fork

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,7 +11,6 @@ import './global/hbsPartials';
 import './global/user';
 import { prefetch } from './global/ld-store';
 import './global/item-cache';
-import './global/history-notfound-trigger';
 import './global/user-semantic-queries';
 import './aspects/navigation';
 import './aspects/authentication';
